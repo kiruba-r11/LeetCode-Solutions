a13ee2c31424419f809a543bd269b8e95e4a55e2
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-01-30 23:50:08 +0530

    Time: 244 ms (86.05%), Space: 99.5 MB (86.19%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
+        
+        /*
+        
+        O(N) time and space
+        Idea -> Sliding Window (Variable Window Size -> Max. size of window is the answer) + Hash
+        
+        */
+        
         int i = 0 , j = 0 , n = nums.size() , sum = 0 , maxsum = 0;
         unordered_map <int , int> hash;
         
         while(j < n) {
-            // sum = 0;
-            // cnt = 0;
+            
+            // If the element is not found before, then its the unique one in the subarray
             while(j < n && hash[nums[j]] == 0) {
                 sum += nums[j];
                 hash[nums[j]]++;
                 j++;
-                // cnt++;
             }
+            
+            // The above loop stops if it reached out of bound or a dupliacte is found. So, by then we would have got our maxsum
             maxsum = max(sum , maxsum);
+            
+            // If the duplicate is found, make sure to remove elements from the left of the subarray until the required duplicate is removed
             while(i < j && j < n && hash[nums[j]] == 1) {
                 sum -= nums[i];
                 hash[nums[i]]--;
                 i++;
             }
         }
+        
         return maxsum;
     }
 };
