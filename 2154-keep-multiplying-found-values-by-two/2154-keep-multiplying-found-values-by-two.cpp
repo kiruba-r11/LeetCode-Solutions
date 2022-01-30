a99f05987c4dba0af00744796306b476ee7fed19
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-01-30 11:35:01 +0530

    Time: 24 ms (10.00%), Space: 10.6 MB (10.00%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -7,8 +7,6 @@ public:
         O(N) time and space
         Idea -> Hashing
         
-        */
-        
         unordered_map <int , int> hash;
         
         // Store the occurence of every number
@@ -22,6 +20,26 @@ public:
             // Else return it
             else break;
         }
+        return original;
+        
+        */
+        
+        /*
+        
+        O(NlogN) time and O(1) space
+        Idea -> Sorting
+        
+        */
+        
+        sort(nums.begin() , nums.end());
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            
+            // If original is found, double it. Since the array is sorted, double of original will be found only after this current index if it exisited in the array
+            if(nums[i] == original) original *= 2;
+        }
+        
         return original;
     }
 };
