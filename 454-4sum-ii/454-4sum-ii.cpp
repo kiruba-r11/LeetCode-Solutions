3b351aaa11f55ff809a45f7374a08b057b61731e
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-02-03 19:27:35 +0530

    Time: 345 ms (40.23%), Space: 27.8 MB (8.06%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -68,7 +68,7 @@ public:
 //         O(N^3 * logN) time and O(1) space
 //         Idea -> For every triplet, binary search an element which gives the total sum of tuple as 0
         
-//         // TLE, I really dont know WHY? 
+//         TLE, I really dont know WHY? 
         
 //         */
         
@@ -106,12 +106,12 @@ public:
 //             }
 //         }
         
-        /*
+//         /*
         
 //         O(N^3) time and O(N) space
 //         Idea -> For every triplet, find the count of the element in fourth array which gives the total sum of tuple as 0
         
-//         // TLE, I really dont know WHY? 
+//         TLE, I really dont know WHY? 
         
 //         */
         
@@ -147,6 +147,142 @@ public:
 //             }
 //         }
         
+
+//         /*
+        
+//         O(N^3) time and O(1) space
+//         Idea -> For every doublet, find a pair using two sum technique such that the tuple sum is 0
+        
+//         TLE, I really dont know WHY? 
+        
+//         */
+        
+//         // Sort the array
+//         sort(nums1.begin() , nums1.end());
+//         sort(nums2.begin() , nums2.end());
+//         sort(nums3.begin() , nums3.end());
+//         sort(nums4.begin() , nums4.end());
+        
+//         int n1 = nums1.size() , n2 = nums2.size() , n3 = nums3.size() , n4 = nums4.size() , count = 0;
+        
+//         // Iterate through the first array, for every element, check if there is a triplet
+//         for(int i = 0; i < n1; i++) {
+            
+//             int temp_i = i + 1;
+            
+//             // Skipping consecutive elements
+//             while(temp_i < n1 && nums1[temp_i] == nums1[i]) temp_i++;
+            
+//             int temp_i_count = 0;
+            
+//             // Iterate through the second array, for every element, check if there is a pair
+//             for(int j = 0; j < n2; j++) {
+
+//                 int temp_j = j + 1;
+                
+//                 // Skipping consecutive elements
+//                 while(temp_j < n2 && nums2[temp_j] == nums2[j]) temp_j++;
+                
+//                 int sum = nums1[i] + nums2[j];
+//                 int low = 0 , high = n4 - 1;
+                
+//                 int temp_j_count = 0;
+                
+//                 // Just a two sum technique
+//                 while(low < n3 && high >= 0) {
+//                     int cursum = nums3[low] + nums4[high];
+                    
+//                     if(sum + cursum == 0) {
+                        
+//                         int temp_low = low + 1;
+//                         int temp_high = high - 1;
+                        
+//                         // Skipping consecutive elements
+//                         while(temp_low < n3 && nums3[temp_low] == nums3[low]) temp_low++;
+                        
+//                         // Skipping consecutive elements
+//                         while(temp_high >= 0 && nums4[temp_high] == nums4[high]) temp_high--;
+                        
+//                         temp_j_count += abs(temp_low - low) * abs(high - temp_high);
+                        
+//                         low = temp_low;
+//                         high = temp_high;
+                        
+//                     } else if(sum + cursum > 0) {
+                        
+//                         int temp_high = high - 1;
+                        
+//                         // Skipping consecutive elements
+//                         while(temp_high >= 0 && nums4[temp_high] == nums4[high]) temp_high--;
+                        
+//                         high = temp_high;
+//                     } else {
+//                         int temp_low = low + 1;
+                        
+//                         // Skipping consecutive elements
+//                         while(temp_low < n3 && nums3[temp_low] == nums3[low]) temp_low++;
+                        
+//                         low = temp_low;
+//                     }
+//                 }
+
+//                 temp_i_count += temp_j_count * abs(temp_j - j);
+//                 j = temp_j - 1;
+//             }
+            
+//             count += temp_i_count * abs(temp_i - i);
+//             i = temp_i - 1;
+//         }
+       
+        
+//         /*
+        
+//         O(N^2) time and O(N) space
+//         Idea -> For every doublet, find the count of the element for every possible sum in third and fourth array which gives the total sum of tuple as 0
+        
+//         TC passed FINALLY!
+        
+//         */
+        
+        
+//         // Sort the array
+//         sort(nums1.begin() , nums1.end());
+//         sort(nums2.begin() , nums2.end());
+//         sort(nums3.begin() , nums3.end());
+//         sort(nums4.begin() , nums4.end());
+        
+//         int n1 = nums1.size() , n2 = nums2.size() , n3 = nums3.size() , n4 = nums4.size() , count = 0;
+//         unordered_map <int , int> hash;
+        
+//         // Store all the possible sums of elements in third and fourth array
+//         for(int i = 0; i < n3; i++) {
+//             for(int j = 0; j < n4; j++) {
+//                 hash[nums3[i] + nums4[j]]++;
+//             }
+//         }
+        
+//         // Iterate through the first array, for every element, check if there is a triplet
+//         for(int i = 0; i < n1; i++) {
+            
+//             // Iterate through the second array, for every element, check if there is a pair
+//             for(int j = 0; j < n2; j++) {
+                    
+//                 int sum = nums1[i] + nums2[j];
+
+//                 // Find the occurences of -sum in fourth array
+//                 count += hash[-1 * sum];
+
+//             }
+//         }
+        
+//         /*
+        
+//         O(N) time and O(N) space
+//         Idea -> For pair sum in first and second array, find the count of pair sum in third and fourth array which gives the total sum of tuple as 0
+        
+//         TC passed FINALLY! The MOST OPTIMAL SOLUTION POSSIBLE!
+        
+//         */ 
         
         // Sort the array
         sort(nums1.begin() , nums1.end());
@@ -155,29 +291,28 @@ public:
         sort(nums4.begin() , nums4.end());
         
         int n1 = nums1.size() , n2 = nums2.size() , n3 = nums3.size() , n4 = nums4.size() , count = 0;
-        unordered_map <int , int> hash;
+        unordered_map <int , int> hash1 , hash2;
+        
+         // Store all the possible sums of elements in first and second array
+        for(int i = 0; i < n1; i++) {
+            for(int j = 0; j < n2; j++) {
+                hash1[nums1[i] + nums2[j]]++;
+            }
+        }
         
         // Store all the possible sums of elements in third and fourth array
         for(int i = 0; i < n3; i++) {
             for(int j = 0; j < n4; j++) {
-                hash[nums3[i] + nums4[j]]++;
+                hash2[nums3[i] + nums4[j]]++;
             }
         }
         
-        // Iterate through the first array, for every element, check if there is a triplet
-        for(int i = 0; i < n1; i++) {
+        // Iterate through the first hash, for every pair sum of first and second array elements, check if there is a pair sum of third and fourth array elements such that the tuple sum is 0
+        for(auto it: hash1) {
             
-            // Iterate through the second array, for every element, check if there is a pair
-            for(int j = 0; j < n2; j++) {
-                    
-                int sum = nums1[i] + nums2[j];
-
-                // Find the occurences of -sum in fourth array
-                count += hash[-1 * sum];
-
-            }
+            count += hash1[it.first] * hash2[-1 * it.first];
         }
-        
+         
         return count;
     }
 };
