01726a04ac7ca2ed45eeaf9a4d914a9897fa2230
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-02-21 20:49:08 +0530

    Time: 26 ms (53.29%), Space: 19.7 MB (9.42%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // Brute Force -> O(n) time and O(n) space
+        map <int , int> m;
+        for(int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        for(auto it = m.begin(); it != m.end(); it++) {
+            if(it->second > nums.size() / 2)
+                return it->first;
+        }
+        return -1;
+    }
+};
