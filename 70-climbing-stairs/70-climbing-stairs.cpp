fefceb625bc1985253cf57866670f096f0aea514
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-03-01 14:35:42 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.35%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -14,16 +14,37 @@ public:
         return dp[n] = climbOne + climbTwo;
     }
     
+    int solve(int n) {
+        
+        vector <int> dp(n + 1);
+        
+        dp[0] = 1;
+        
+        for(int i = 1; i <= n; i++) {
+            int climbOne = dp[i - 1];
+            int climbTwo = i >= 2 ? dp[i - 2] : 0;
+            
+            dp[i] = climbOne + climbTwo;
+        }
+        
+        return dp[n];
+    }
+    
     int climbStairs(int n) {
         
         /*
         
         Memoization:
             O(N) time and O(N + N) space complexity
+            
+        Tabulation:
+            O(N) time and O(N) space complexity
         
         */
         
-        vector <int> dp(n + 1 , -1);
-        return solve(n , dp);
+//         vector <int> dp(n + 1 , -1);
+//         return solve(n , dp);
+        
+        return solve(n);
     }
 };
