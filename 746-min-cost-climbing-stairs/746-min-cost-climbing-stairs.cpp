ffb1ea1c6c63218aa9a58c690e689c63caf29ce0
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-03-01 15:34:49 +0530

    Time: 8 ms (65.21%), Space: 13.7 MB (49.93%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -13,21 +13,44 @@ public:
         return dp[index] = min(climbOne , climbTwo);
     }
     
+    int solve(vector <int> &cost) {
+        
+        int n = cost.size();
+        vector <int> dp(n);
+        
+        for(int i = n - 1; i >= 0; i--) {
+            
+            int climbOne = cost[i] , climbTwo = cost[i];
+            
+            if(i + 1 < n) climbOne += dp[i + 1];
+            if(i + 2 < n) climbTwo += dp[i + 2];
+            
+            dp[i] = min(climbOne , climbTwo);
+        }
+        
+        return min(dp[0] , dp[1]);
+    }
+    
     int minCostClimbingStairs(vector<int>& cost) {
         
         /*
         
         Memoization:
             O(N) time and O(N + N) space complexity
+            
+        Tabulation:
+            O(N) time and O(N) space complexity
         
         */
         
-        int n = cost.size();
-        vector <int> dp(n , -1);
+//         int n = cost.size();
+//         vector <int> dp(n , -1);
             
-        int startZero = solve(cost , 0 , dp);
-        int startOne = solve(cost , 1 , dp);
+//         int startZero = solve(cost , 0 , dp);
+//         int startOne = solve(cost , 1 , dp);
               
-        return min(startZero , startOne);
+//         return min(startZero , startOne);
+        
+        return solve(cost);
     }
 };
