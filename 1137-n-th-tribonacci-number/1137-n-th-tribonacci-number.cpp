732e07988765e09984c31a003eefd140a1b27d33
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-03-01 14:26:31 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (29.92%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     int trib(int n , vector <int> &dp) {
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
@@ -8,16 +9,40 @@ public:
         
         return dp[n] = trib(n - 1 , dp) + trib(n - 2 , dp) + trib(n - 3 , dp);
     }
+    
+    int trib(int n) {
+        
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        
+        vector <int> dp(n + 1);
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        
+        return dp[n];
+    }
+    
     int tribonacci(int n) {
         
         /*
         
         Memoization:
             O(N) time and O(N + N) space complexity
+            
+        Tabulation:
+            O(N) time and O(N) space complexity
         
         */
         
-        vector <int> dp(n + 1 , -1);
-        return trib(n , dp);
+        // vector <int> dp(n + 1 , -1);
+        // return trib(n , dp);
+        
+        return trib(n);
     }
 };
