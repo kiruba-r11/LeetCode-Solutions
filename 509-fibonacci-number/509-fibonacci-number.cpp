b8938b25e60e3517e98bdab43f010ef06654d5cb
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-03-01 14:19:40 +0530

    Time: 4 ms (52.91%), Space: 6.1 MB (16.46%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
+    
     int fibonacci(int n , vector <int> &dp) {
         if(n <= 1) return n;
         return dp[n] = fibonacci(n - 1 , dp) + fibonacci(n - 2 , dp);
     }
+    
+    int fibonacci(int n) {
+        
+        if(n == 0) return 0;
+        
+        vector <int> dp(n + 1);
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+    
     int fib(int n) {
         
         /*
         
         Memoization:
             O(N) time and O(N + N) space complexity
-        
+            
+        Tabulation: 
+            O(N) time and O(N) space complexity
+         
         */
         
-        vector <int> dp(n + 1 , -1);
-        return fibonacci(n , dp);
+        // vector <int> dp(n + 1 , -1);
+        // return fibonacci(n , dp);
+        
+        return fibonacci(n);
     }
 };
