b8e9e0ee549c7e1166b0c5117e4d3d753639a207
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-03-07 12:10:42 +0530

    Time: 28 ms (98.56%), Space: 32.4 MB (63.10%) - LeetHub

---
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* merge(TreeNode* root1 , TreeNode* root2) {
+        if(root1 == NULL && root2 == NULL) return NULL;
+        if(root1 == NULL || root2 == NULL) return root1 ? root1 : root2;
+        
+        root1->val += root2->val;
+        root1->left = merge(root1->left , root2->left);
+        root1->right = merge(root1->right , root2->right);
+        
+        return root1;
+    }
+    
+    
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        
+        /*
+        
+        O(N) time and space (stack/queue) complexity
+        Idea -> DFS 
+        
+        */
+        
+        return merge(root1 , root2);
+    }
+};
