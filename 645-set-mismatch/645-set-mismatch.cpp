a768e61e4e214e3b03f2eb128170aab0eb881055
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-03-07 14:39:39 +0530

    Time: 29 ms (85.16%), Space: 21.2 MB (96.29%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int _xor = 0 , summ = 0 , sum = 0;
+        int n = nums.size();
+        
+        for(int i = 1; i <= n; i++) {
+            _xor ^= i;
+            summ += i;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            _xor ^= nums[i];
+            sum += nums[i];
+        }
+        
+        int last = 0;
+        while(true) {
+            if(_xor & (1 << last)) break;
+            last++;
+        }
+        
+        int num1 = 0 , num2 = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            if(i & (1 << last)) {
+                num1 ^= i;
+            } else {
+                num2 ^= i;
+            }
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(nums[i] & (1 << last)) {
+                num1 ^= nums[i];
+            } else {
+                num2 ^= nums[i];
+            }
+        }
+        
+        bool check = false;
+        for(int i = 0; i < n; i++) {
+            if(num1 == nums[i]) {
+                check = true;
+                break;
+            }
+        }
+        
+        if(check) return {num1 , num2};
+        return {num2 , num1};
+    }
+};
