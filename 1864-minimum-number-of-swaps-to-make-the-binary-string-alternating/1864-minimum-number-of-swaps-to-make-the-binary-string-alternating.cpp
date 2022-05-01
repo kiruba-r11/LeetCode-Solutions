d3385f62fe83308845e9c9a1fa60d6c89a3d3ca8
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-05-01 15:29:14 +0530

    Time: 6 ms (22.12%), Space: 6.5 MB (70.19%) - LeetHub

--- 1864-minimum-number-of-swaps-to-make-the-binary-string-alternating/1864-minimum-number-of-swaps-to-make-the-binary-string-alternating.cpp
+++ 1864-minimum-number-of-swaps-to-make-the-binary-string-alternating/1864-minimum-number-of-swaps-to-make-the-binary-string-alternating.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    int helper(string &s , char ch1 , char ch2 , int c) {
+        
+        int cnt = c , n = s.size();
+        for(int i = 1; i < n; i++) {
+            if(i & 1) {
+                if(s[i] != ch1) cnt++;
+            } else {
+                if(s[i] != ch2) cnt++;
+            }
+        }
+        
+        return cnt;
+    }
     int minSwaps(string s) {
         int n = s.size();
         int ones = 0 , zeroes = 0;
@@ -14,56 +27,24 @@ public:
         vector <int> cnt(4 , 1e7);
         
         if(s[0] == '1') {
-            
-            
             // starting with one, and keep that
-            cnt[0] = 0;
-            for(int i = 1; i < n; i++) {
-                if(i & 1) {
-                    if(s[i] != '0') cnt[0]++;
-                } else {
-                    if(s[i] != '1') cnt[0]++;
-                }
-            }
+            cnt[0] = helper(s , '0' , '1' , 0);
 
             // starting with one, and change that
-            cnt[1] = 1;
-            for(int i = 1; i < n; i++) {
-                if(i & 1) {
-                    if(s[i] != '1') cnt[1]++;
-                } else {
-                    if(s[i] != '0') cnt[1]++;
-                }
-            }
+            cnt[1] = helper(s , '1' , '0' , 1);
         } else {
             
             // starting with zero, and keep that
-            cnt[2] = 0;
-            for(int i = 1; i < n; i++) {
-                if(i & 1) {
-                    if(s[i] != '1') cnt[2]++;
-                } else {
-                    if(s[i] != '0') cnt[2]++;
-                }
-            }
+            cnt[2] = helper(s , '1' , '0' , 0);
 
             // starting with zero, and change that
-            cnt[3] = 1;
-            for(int i = 1; i < n; i++) {
-                if(i & 1) {
-                    if(s[i] != '0') cnt[3]++;
-                } else {
-                    if(s[i] != '1') cnt[3]++;
-                }
-            }
+            cnt[3] = helper(s , '0' , '1' , 1);
         }
         
         
         int ans = 1e7;
         for(auto i: cnt) {
-            // cout << i << " ";
-            if(!(i & 1))
-                ans = min(ans , i / 2);
+            if(!(i & 1)) ans = min(ans , i / 2);
         }
         
         return ans;
