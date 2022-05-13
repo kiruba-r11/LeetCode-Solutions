0d9c9673283ac01f1179d471b98864666d76e50e
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-05-13 10:24:46 +0530

    Time: 197 ms (27.88%), Space: 12.8 MB (98.02%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,7 +1,5 @@
 class Solution {
 public:
-    int dp[1002][1002];
-    
     static bool cmp(string &a , string &b) {
         if(a.size() == b.size()) 
             return a < b;
@@ -28,48 +26,52 @@ public:
         return true;
     }
     
-    int solve(vector <string> &words , int index , int select) {
+//     int solve(vector <string> &words , int index , int select) {
     
-        if(words.size() + 1 == index) return 0;
+//         if(words.size() + 1 == index) return 0;
         
-        if(dp[index][select] != -1) return dp[index][select];
+//         if(dp[index][select] != -1) return dp[index][select];
         
-        int pick = 0 , notPick = 0;
-        notPick = solve(words , index + 1 , select);
+//         int pick = 0 , notPick = 0;
+//         notPick = solve(words , index + 1 , select);
 
-        if(select == 0 || isPredecessor(words[select - 1] , words[index - 1]))
-            pick = 1 + solve(words , index + 1 , index);
+//         if(select == 0 || isPredecessor(words[select - 1] , words[index - 1]))
+//             pick = 1 + solve(words , index + 1 , index);
         
-        return dp[index][select] = max(pick , notPick);
+//         return dp[index][select] = max(pick , notPick);
+//     }
+    
+    void move(int *next , int *cur , int n) {
+        for(int i = 0; i <= n; i++) {
+            next[i] = cur[i];
+        }
     }
     
     int solve(vector <string> &words) {
         
         int n = words.size();
+        int cur[n + 1] , next[n + 1];
+        
+        for(int i = 0; i <= n; i++) {
+            cur[i] = next[i] = 0;
+        }
         
         for(int i = n; i >= 1; i--) {
             for(int j = 0; j <= i; j++) {
-                int notPick = dp[i + 1][j];
+                int notPick = next[j];
                 int pick = 0;
                 
-                if(j == 0 || isPredecessor(words[j - 1] , words[i - 1])) pick = 1 + dp[i + 1][i];
+                if(j == 0 || isPredecessor(words[j - 1] , words[i - 1])) pick = 1 + next[i];
                 
-                dp[i][j] = max(pick , notPick);
+                cur[j] = max(pick , notPick);
             }
+            move(next , cur , n);
         }
         
-        return dp[1][0];
+        return next[0];
     }
     
     int longestStrChain(vector<string>& words) {
-        
-        int n = words.size();
-        
-        for(int i = 0; i <= 1001; i++) {
-            for(int j = 0; j <= 1001; j++) {
-                dp[i][j] = 0;
-            }
-        }
         
         sort(words.begin() , words.end() , cmp);
         return solve(words);
