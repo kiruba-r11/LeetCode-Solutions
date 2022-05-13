b14ca5880d18ddefffac28f553ef84dc12639c61
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-05-13 10:11:48 +0530

    Time: 853 ms (5.03%), Space: 159.2 MB (5.50%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -2,26 +2,46 @@ class Solution {
 public:
     int solve(vector <vector <int>> &pairs , int index , int select , vector <vector <int>> &dp) {
  
-        if(index == pairs.size()) return 0;
+        if(index == pairs.size() + 1) return 0;
         
-        if(dp[index][select + 1] != -1) return dp[index][select + 1];
+        if(dp[index][select] != -1) return dp[index][select];
         
         int pick = 0 , notPick = 0;
         
         notPick = solve(pairs , index + 1 , select , dp);
 
-        if(select == -1 || pairs[select][1] < pairs[index][0]) 
+        if(select == 0 || pairs[select - 1][1] < pairs[index - 1][0]) 
             pick = 1 + solve(pairs , index + 1 , index , dp);
         
-        return dp[index][select + 1] = max(pick , notPick);
+        return dp[index][select] = max(pick , notPick);
         
     }
+    
+    int solve(vector <vector <int>> &pairs) {
+        
+        int n = pairs.size();
+        
+        vector <vector <int>> dp(n + 1 , vector <int> (n + 1));
+        
+        for(int i = n - 1; i >= 1; i--) {
+            for(int j = 0; j <= i; j++) {
+                int pick = 0 , notPick = 0;
+                
+                notPick = dp[i + 1][j];
+                if(j == 0 || pairs[j][1] < pairs[i][0]) pick = 1 + dp[i + 1][i];
+                
+                dp[i][j] = max(pick , notPick);
+            }
+        }
+        
+        return dp[0][1];
+    }
+    
     int findLongestChain(vector<vector<int>>& pairs) {
         sort(pairs.begin() , pairs.end());
-        
-        int n = pairs.size() , m = pairs.size();
-        
-        vector <vector <int>> dp(n , vector <int> (m + 1 , -1));
-        return solve(pairs , 0 , -1 , dp);
+        int n = pairs.size();
+        vector <vector <int>> dp(n + 1 , vector <int> (n + 1 , -1));
+        return solve(pairs , 1 , 0 , dp);
+        // return solve(pairs);
     }
 };
