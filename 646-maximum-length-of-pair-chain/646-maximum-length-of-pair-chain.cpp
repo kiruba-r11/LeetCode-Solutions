ceee84f1d805063f290123085f7dac58b688f9bb
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-05-13 10:12:48 +0530

    Time: 633 ms (6.42%), Space: 159.3 MB (5.50%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -21,27 +21,27 @@ public:
         
         int n = pairs.size();
         
-        vector <vector <int>> dp(n + 1 , vector <int> (n + 1));
+        vector <vector <int>> dp(n + 2 , vector <int> (n + 1));
         
-        for(int i = n - 1; i >= 1; i--) {
+        for(int i = n; i >= 1; i--) {
             for(int j = 0; j <= i; j++) {
                 int pick = 0 , notPick = 0;
                 
                 notPick = dp[i + 1][j];
-                if(j == 0 || pairs[j][1] < pairs[i][0]) pick = 1 + dp[i + 1][i];
+                if(j == 0 || pairs[j - 1][1] < pairs[i - 1][0]) pick = 1 + dp[i + 1][i];
                 
                 dp[i][j] = max(pick , notPick);
             }
         }
         
-        return dp[0][1];
+        return dp[1][0];
     }
     
     int findLongestChain(vector<vector<int>>& pairs) {
         sort(pairs.begin() , pairs.end());
-        int n = pairs.size();
-        vector <vector <int>> dp(n + 1 , vector <int> (n + 1 , -1));
-        return solve(pairs , 1 , 0 , dp);
-        // return solve(pairs);
+        // int n = pairs.size();
+        // vector <vector <int>> dp(n + 1 , vector <int> (n + 1 , -1));
+        // return solve(pairs , 1 , 0 , dp);
+        return solve(pairs);
     }
 };
