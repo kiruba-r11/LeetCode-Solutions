c4766d5f8dad1e4ea1010dccebd88d26d57db28f
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-05-22 10:38:40 +0530

    Time: 762 ms (33.33%), Space: 100.8 MB (33.33%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -6,15 +6,26 @@ public:
         if(n <= 2) return n - 1;
         
         sort(nums.begin() , nums.end());
-        long double slope = (long double)(nums[1][1] - nums[0][1]) / (nums[1][0] - nums[0][0]);
+        int dx = nums[1][0] - nums[0][0];
+        int dy = nums[1][1] - nums[0][1];
+        int gcd = __gcd(dx , dy);
+        
+        dx /= gcd;
+        dy /= gcd;
         
         int ans = 1;
         for(int i = 2; i < n; i++) {
-            long double newSlope = 
-                (long double)(nums[i][1] - nums[i - 1][1]) / (nums[i][0] - nums[i - 1][0]);
-            if(newSlope != slope) {
+            int ndx = nums[i][0] - nums[i - 1][0];
+            int ndy = nums[i][1] - nums[i - 1][1];
+            int ngcd = __gcd(ndx , ndy);
+            
+            ndx /= ngcd;
+            ndy /= ngcd;
+            
+            if(!(dx == ndx && dy == ndy)) {
                 ans++;
-                slope = newSlope;
+                dx = ndx;
+                dy = ndy;
             }
         }
         
