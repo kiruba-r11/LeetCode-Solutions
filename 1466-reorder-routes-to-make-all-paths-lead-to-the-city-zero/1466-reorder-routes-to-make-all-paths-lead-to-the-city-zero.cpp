87777d54d4ac93c272f7b69171714183171b7928
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-06-24 18:47:46 +0530

    Time: 1541 ms (5.00%), Space: 176.2 MB (7.67%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    void solve(vector <vector <int>> &graph , int src , int &ans , map <vector <int> , bool> &hash , vector <bool> &visited , vector <bool> &reach) {
+    
+        if(visited[src]) return;
+        
+        visited[src] = true;
+        for(int i = 0; i < graph[src].size(); i++) {
+            int newSrc = graph[src][i];
+            
+            // if(!reach[newSrc]) {
+            //     ans++;
+            //     reach[newSrc] = true;
+            // } else {
+            //     reach[src] = true;
+            // }
+
+            if(reach[newSrc] && hash.find({src , newSrc}) == hash.end()) {
+                reach[src] = true;
+                // cout << src << " " << newSrc << endl;
+                ans++;
+            } else if(hash.find({src , newSrc}) != hash.end()) {
+                reach[src] = true;
+            }
+            
+            // solve(graph , newSrc , ans , reach , visited);
+            solve(graph , newSrc , ans , hash , visited , reach);
+        }
+        
+    }
+    
+    int minReorder(int m, vector<vector<int>>& connections) {
+        int n = connections.size();
+        
+        vector <vector <int>> graph(m);
+        vector <bool> reach(m , false) , visited(m , false);
+        map <vector <int> , bool> hash;
+        reach[0] = true;
+        
+        
+        for(int i = 0; i < n; i++) {
+            graph[connections[i][0]].push_back(connections[i][1]);
+            graph[connections[i][1]].push_back(connections[i][0]);
+            hash[{connections[i][0] , connections[i][1]}]= true;
+        }
+        
+        int ans = 0;
+        // for(int i = 0; i < m; i++) {
+            // if(visited[i] == false)
+        solve(graph , 0 , ans , hash , visited , reach);
+        // }
+        
+        return ans;
+    }
+};
+
+// 3 -> 2 -> 0 -> 1
+// 1 -> 2 -> 0
