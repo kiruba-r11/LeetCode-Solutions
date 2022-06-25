0504bc82b13c9b448dfe98572a435e0f85275157
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-06-25 23:01:39 +0530

    Time: 502 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

--- 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
+++ 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-    int dp[10001][7][7];
+    int cur[7][7] , prev[7][7];
     int mod = 1e9 + 7;
-    int solve(int n , int prev1 , int prev2) {
+//     int solve(int n , int prev1 , int prev2) {
         
-        if(n == 0) return 1;
+//         if(n == 0) return 1;
         
-        if(dp[n][prev1][prev2] != -1) return dp[n][prev1][prev2];
+//         if(dp[n][prev1][prev2] != -1) return dp[n][prev1][prev2];
         
-        int ans = 0;
-        for(int i = 1; i <= 6; i++) {
-            if(i != prev1 && i != prev2 && (prev2 == 0 || __gcd(i , prev2) == 1)) {
-                ans = (ans + solve(n - 1 , prev2 , i)) % mod;
+//         int ans = 0;
+//         for(int i = 1; i <= 6; i++) {
+//             if(i != prev1 && i != prev2 && (prev2 == 0 || __gcd(i , prev2) == 1)) {
+//                 ans = (ans + solve(n - 1 , prev2 , i)) % mod;
+//             }
+//         }
+        
+//         return dp[n][prev1][prev2] = ans;
+//     }
+    
+    void move(int cur[7][7] , int prev[7][7]) {
+        
+        for(int i = 0; i <= 6; i++) {
+            for(int j = 0; j <= 6; j++) {
+                prev[i][j] = cur[i][j];
             }
         }
-        
-        return dp[n][prev1][prev2] = ans;
     }
     
     int solve(int n) {
         
         for(int i = 0; i <= 6; i++) {
             for(int j = 0; j <= 6; j++) {
-                dp[0][i][j] = 1;
+                cur[i][j] = 1;
+                prev[i][j] = 1;
             }
         }
         
@@ -32,25 +42,27 @@ public:
                     int ans = 0;
                     for(int m = 1; m <= 6; m++) {
                         if(m != j && m != k && (k == 0 || __gcd(m, k) == 1)) {
-                            ans = (ans + dp[i - 1][k][m]) % mod;
+                            ans = (ans + prev[k][m]) % mod;
                         }
                     }
-                    dp[i][j][k] = ans;
+                    cur[j][k] = ans;
                 }
             }
+            move(cur , prev);
         }
         
-        return dp[n][0][0];
+        return prev[0][0];
     }
     
     int distinctSequences(int n) {
-        for(int i = 0; i <= 10000; i++) {
-            for(int j = 0; j <= 6; j++) {
-                for(int k = 0; k <= 6; k++) {
-                    dp[i][j][k] = -1;
-                }
-            }
-        }
+        // for(int i = 0; i <= 10000; i++) {
+            // for(int j = 0; j <= 6; j++) {
+                // for(int k = 0; k <= 6; k++) {
+                    // cur[j][k] = -1;
+                    // prev[j][k] = -1;
+                // }
+            // }
+        // }
         // return solve(n , 0 , 0);
         return solve(n);
     }
