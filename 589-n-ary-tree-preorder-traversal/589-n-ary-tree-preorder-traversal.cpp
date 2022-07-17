91e24670014b790ba5862477fca511f98bfe5268
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-07-17 21:40:59 +0530

    Time: 34 ms (54.03%), Space: 11.2 MB (50.68%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -20,42 +20,18 @@ public:
 
 class Solution {
 public:
-    void preOrder(Node* root , vector <int> &pre) {
-        if(root == NULL) return;
+    void solve(Node* root , vector <int> &ans) {
+        if(!root) return ;
         
-        pre.push_back(root->val);
-        
-        for(auto child: root->children) {
-            preOrder(child , pre);     
+        ans.push_back(root->val);
+        for(auto i: root->children) {
+            solve(i , ans);
         }
     }
     
     vector<int> preorder(Node* root) {
-        
-        /*
-    
-        O(N) time and O(H) space (stack) complexity
-
-        */
-        
-        vector <int> pre;
-        
-        preOrder(root , pre);
-        
-        return pre;
+        vector <int> ans;
+        solve(root , ans);
+        return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
