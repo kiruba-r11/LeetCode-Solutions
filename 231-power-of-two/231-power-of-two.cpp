108cd6fce92993a02d665e46716c8cfebc904329
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-07-18 19:02:57 +0530

    Time: 7 ms (14.71%), Space: 5.9 MB (69.98%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
-        /*
-        
-        O(No. of bits) time and O(1) space complexity
-        Idea -> Brian Kernighan's Algorithm
-        
-        */
-        
+        int cnt = 0;
         if(n <= 0) return false;
-        
-        int bits = 0;
-        
         while(n) {
+            cnt++;
             n = n & (n - 1);
-            bits++;
         }
-        
-        return bits == 1;
+        return cnt == 1;
     }
 };
