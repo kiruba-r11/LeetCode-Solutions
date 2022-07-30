fcfee6c0d65876778e4ddd4f0071fb9299193e3b
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-07-30 23:39:02 +0530

    Time: 201 ms (32.63%), Space: 13.1 MB (90.42%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -26,54 +26,25 @@ public:
         return true;
     }
     
-//     int solve(vector <string> &words , int index , int select) {
-    
-//         if(words.size() + 1 == index) return 0;
-        
-//         if(dp[index][select] != -1) return dp[index][select];
-        
-//         int pick = 0 , notPick = 0;
-//         notPick = solve(words , index + 1 , select);
-
-//         if(select == 0 || isPredecessor(words[select - 1] , words[index - 1]))
-//             pick = 1 + solve(words , index + 1 , index);
-        
-//         return dp[index][select] = max(pick , notPick);
-//     }
-    
-    void move(int *next , int *cur , int n) {
-        for(int i = 0; i <= n; i++) {
-            next[i] = cur[i];
-        }
-    }
-    
-    int solve(vector <string> &words) {
-        
-        int n = words.size();
-        int cur[n + 1] , next[n + 1];
-        
-        for(int i = 0; i <= n; i++) {
-            cur[i] = next[i] = 0;
-        }
-        
-        for(int i = n; i >= 1; i--) {
-            for(int j = 0; j <= i; j++) {
-                int notPick = next[j];
-                int pick = 0;
-                
-                if(j == 0 || isPredecessor(words[j - 1] , words[i - 1])) pick = 1 + next[i];
-                
-                cur[j] = max(pick , notPick);
-            }
-            move(next , cur , n);
-        }
-        
-        return next[0];
-    }
     
     int longestStrChain(vector<string>& words) {
-        
+       
+        int n = words.size();
+        vector <int> dp(n , 1);
+    
         sort(words.begin() , words.end() , cmp);
-        return solve(words);
+        
+        int len = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(isPredecessor(words[j] , words[i])) {
+                    dp[i] = max(dp[i] , 1 + dp[j]);
+                }
+            }
+            
+            len = max(len , dp[i]);
+        }
+        
+        return len;
     }
 };
