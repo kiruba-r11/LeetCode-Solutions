8c3f986e0c46ebc6e484c732969a1b77e50f3e2a
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-08-08 21:44:20 +0530

    Time: 673 ms (21.86%), Space: 10.6 MB (28.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,17 +3,31 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size() , len = 0;
         vector <int> dp(n , 1);
+        vector <int> hash(n);
         
+        int idx = -1;
         for(int i = 0; i < n; i++) {
+            hash[i] = i;
             for(int j = 0; j < i; j++) {
                 if(nums[j] < nums[i]) {
-                    dp[i] = max(dp[i] , 1 + dp[j]);
+                    if(dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        hash[i] = j;
+                    }
                 }
             }
             
-            len = max(len , dp[i]);
+            if(len <= dp[i]) {
+                len = dp[i];
+                idx = i;
+            }
         }
         
+        while(idx != hash[idx]) {
+            cout << nums[idx] << ' ';
+            idx = hash[idx];
+        }
+        cout << nums[idx] << " " << endl;
         return len;
     }
 };
