ff25c767df99f11fa2d1a8c9db1fda3c5213dad0
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2022-09-13 20:01:58 +0530

    Time: 47 ms (7.98%), Space: 15.7 MB (5.74%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     bool validUtf8(vector<int>& data) {
         int n = data.size();
+        
         for(int i = 0; i < n; i++) {
+            
             vector <int> store(8);
             int elem = data[i];
             int j = 7;
+            
             while(elem > 0) {
                 store[j] = elem % 2;
                 elem /= 2;
@@ -14,6 +17,7 @@ public:
             
             int cnt = 0;
             j = 0;
+            
             while(j < 7) {
                 if(store[j] == 1) {
                     cnt++;
@@ -22,25 +26,19 @@ public:
                 }
                 j++;
             }
-            // cout << data[i] << " " << cnt << endl;
+
             if(cnt == 0) continue;
             else if(cnt == 1 || cnt >= 5) return false;
             else {
-                if((n - i - 1) < (cnt - 1)) {
-                    // cout << i << endl;
-                    return false;
-                }
+                if((n - i - 1) < (cnt - 1)) return false;
+                
                 int k = i + 1;
                 while(cnt > 1) {
-                    if(((data[k] & (1 << 7)) != 0) && ((data[k] & (1 << 6)) == 0)) {
-                        // cout << data[k] << " " << (data[k] & (1 << 7)) << endl;
-                        k++;
-                    }
-                    else {
-                        return false;
-                    }
+                    if(((data[k] & (1 << 7)) != 0) && ((data[k] & (1 << 6)) == 0)) k++;
+                    else return false;
                     cnt--;
                 }
+                
                 i = k - 1;
             }
         }
