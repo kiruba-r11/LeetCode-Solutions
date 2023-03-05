4deef8b72f0e2de8d9d0c1fd4d63e420740f2c0d
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2023-03-05 19:17:57 +0530

    Time: 348 ms (50.00%), Space: 72.5 MB (100.00%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int fpow(long long x, unsigned int y, int p) {
-        int res = 1;     
-        x = x % p; 
-        if (x == 0) return 0;
-        while (y > 0) {
-            if (y & 1)
-                res = (res*x) % p;
-            y = y>>1; 
-            x = (x*x) % p;
-        }
-        return res;
-    }
-    
     int countWays(vector<vector<int>>& ranges) {
-        int cnt = 0;
         pair <int , int> s;
-        int n = ranges.size();
         sort(ranges.begin() , ranges.end());
+        
+        int n = ranges.size();
+        int cnt = 0;
+        int ans = 1;
+        int mod = 1e9 + 7;
         
         for(int i = 0; i < n; i++) {
             if(cnt == 0 || s.second < ranges[i][0]) {
                 s = {ranges[i][0] , ranges[i][1]};
                 cnt++;
+                ans = ans * 1L * 2;
+                ans %= mod;
             } else {
                 s.second = max(s.second , ranges[i][1]);
             }
         }
 
-        return fpow(2 , cnt , 1e9 + 7);
+        return ans;
     }
 };
