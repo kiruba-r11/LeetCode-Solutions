087f195dcc33e990a7c962954b84b0ca948322a9
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2023-05-23 10:22:18 +0530

    Time: 441 ms (81.88%), Space: 103.8 MB (42.03%) - LeetHub

---
+++ 1943-describe-the-painting/1943-describe-the-painting.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<vector<long long>> splitPainting(vector<vector<int>>& segments) {
+        int n = segments.size();
+        int m = 0;
+        
+        for(int i = 0; i < n; i++) {
+            m = max(segments[i][1] , m);
+        }
+        
+        vector <pair <long long , int>> prefix(m + 1);
+        for(int i = 0; i < n; i++) {
+            prefix[segments[i][0]].first += segments[i][2];
+            prefix[segments[i][1]].first -= segments[i][2];
+            
+            // start -> 1
+            // end -> 2
+            // start + end -> 3
+            
+            if(prefix[segments[i][0]].second == 2)  
+                prefix[segments[i][0]].second = 3;
+            else if(prefix[segments[i][0]].second != 3)
+                prefix[segments[i][0]].second = 1;
+                
+            
+            if(prefix[segments[i][1]].second == 1)
+                prefix[segments[i][1]].second = 3;
+            else if(prefix[segments[i][1]].second != 3)
+                prefix[segments[i][1]].second = 2;
+        }
+        
+        for(int i = 2; i <= m; i++) {
+            prefix[i].first += prefix[i - 1].first;
+        }
+        
+        vector <vector <long long>> ans;
+        int start = 1;
+//         for(int i = 1; i <= m; i++) {
+//             cout << prefix[i].first << " " << prefix[i].second << endl;
+//         }
+        for(int i = 1; i <= m; i++) {
+            if(prefix[i].first != 0) {
+                start = i;
+                break;
+            }
+        }
+        
+        for(int i = start + 1; i <= m; i++) {
+            if(prefix[i].second != 0) {
+                if(prefix[i - 1].first != 0)
+                    ans.push_back({start , i , prefix[i - 1].first});
+                start = i;
+            }
+        }
+        // cout << endl;
+        
+        return ans;
+    }
+};
