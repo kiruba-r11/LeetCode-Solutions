f65817639f4ea26f9fab3bbe8f25b77fa0c3fb63
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2023-05-23 14:28:28 +0530

    Time: 179 ms (29.49%), Space: 35.3 MB (62.39%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        long long ans = 0;
+        
+        int negcnt = 0 , zerocnt = 0 , pos = INT_MAX , neg = INT_MIN;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] < 0) {
+                    negcnt++;
+                    neg = max(neg , matrix[i][j]);
+                }
+                else if(matrix[i][j] == 0) zerocnt++;
+                else pos = min(pos , matrix[i][j]);
+                ans += abs(matrix[i][j]);
+            }
+        }
+        
+        if(negcnt == n * n) {
+            if(negcnt & 1) ans += 2 * neg;    
+        } else {
+            if(negcnt & 1) {
+                if(zerocnt == 0) {
+                    ans -= 2 * min(abs(neg) , pos);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
