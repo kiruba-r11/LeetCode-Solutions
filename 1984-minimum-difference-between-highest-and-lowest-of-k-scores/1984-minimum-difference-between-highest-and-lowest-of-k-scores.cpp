c741b92fa16d65cf0d182e8eca1832d09a3f2ddb
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2023-06-01 18:26:57 +0530

    Time: 23 ms (19.92%), Space: 13.7 MB (57.26%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -2,30 +2,21 @@ class Solution {
 public:
     int minimumDifference(vector<int>& nums, int k) {
         sort(nums.begin() , nums.end());
-        
-        int n = nums.size() , i = 0 , j = 0 , ans = nums[n - 1];
-        int cur_min = nums[0] , cur_max = nums[0];
-        
-        // The min difference between max and min element of k-sized array can be found by sliding the given array using a window of size k.
+        int i = 0 , j = 0 , n = nums.size();
+        int high = -1 , low = -1;
+        int ans = INT_MAX;
         
         while(j < n) {
             int window = j - i + 1;
-            
             if(window <= k) {
                 if(window == k) {
-                    
-                    // Since array is sorted, the max element of the window will be the last element of the window
-                    cur_max = nums[j];
-                    
-                    // Calculating the ans because the window size is k
-                    ans = min(ans , cur_max - cur_min);
+                    high = nums[j];
+                    low = nums[i];
+                    ans = min(ans , abs(high - low));
                 }
                 j++;
             } else {
                 i++;
-                
-                // Updating the cur_min, as window is slided by 1 position
-                cur_min = nums[i];
             }
         }
         
