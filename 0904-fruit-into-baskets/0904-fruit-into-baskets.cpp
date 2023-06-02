357c8b043e69e0fa40c7bb57a5d9d7c2c94e1da5
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2023-06-02 19:11:38 +0530

    Time: 209 ms (23.97%), Space: 71.2 MB (66.26%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
-    bool slide(vector <int> &nums , int k) {
-    
+    int totalFruit(vector<int>& nums) {
         int n = nums.size();
-        int i = 0 , j = 0;
+        int i = 0 , j = 0 , ans = 0;
         unordered_map <int , int> hash;
         
         while(j < n) {
-            int window = j - i + 1;
-            if(window <= k) {
+            if(hash.size() <= 2) {
                 hash[nums[j]]++;
-                
-                if(window == k) {
-                    if(hash.size() <= 2) return true;
+                if(hash.size() <= 2) ans = max(ans , j - i + 1);
+                j++; 
+            } else {
+                while(hash.size() > 2) {
+                    if(hash[nums[i]] == 1) hash.erase(nums[i]);
+                    else hash[nums[i]]--;
+                    i++;
                 }
-                
-                j++;
-            } else {
-                if(hash[nums[i]] == 1) hash.erase(nums[i]);
-                else hash[nums[i]]--;
-                i++;
-            }
-        }
-        
-        return false;
-    }
-    
-    int totalFruit(vector<int>& fruits) {
-        int n = fruits.size();
-        int low = 0 , high = n , ans = 0;
-        
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            bool check = slide(fruits , mid);
-            if(check) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
             }
         }
         
