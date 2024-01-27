20dc70e6c086918acd1363ba05fa09ea454485ac
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-01-27 20:50:09 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (6.41%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    bool helper(vector <int> &nums , int start) {
-    
+    int minStartValue(vector<int>& nums) {
         int n = nums.size();
-        int prefix = start;
+        int start = 1 , prefix = 1;
+        
         for(int i = 0; i < n; i++) {
             prefix = nums[i] + prefix;
-            if(prefix < 1) return false;
-        }
-        
-        return true;
-    }
-    
-    int minStartValue(vector<int>& nums) {
-        int low = 0 , high = 10001;
-        int ans = 0;
-        
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(helper(nums , mid)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            if(prefix < 1) {
+                start += abs(prefix) + 1;
+                prefix = 1;
             }
         }
         
-        return ans == 0 ? 1 : ans;
+        return start;
     }
 };
