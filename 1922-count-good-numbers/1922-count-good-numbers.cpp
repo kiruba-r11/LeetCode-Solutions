f5700c9b14dbd0648328db77cf1ae75a602e7811
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-01 19:11:55 +0530

    Time: 3 ms (29.95%), Space: 7.1 MB (20.89%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
-    int fpow(long long b , long long p) {
-        if(p == 0) return 1;
-        if(p & 1) return b * fpow((b * b) % mod , p / 2) % mod;
-        return fpow((b * b) % mod , p / 2) % mod;
-    }
-    int countGoodNumbers(long long n) {
-        long long four = n / 2;
-        long long five = n / 2 + n % 2;
+    int mod = 1000000007;
+    long long fpow(long long base , long long power) {
+    
+        if(power == 0) return 1;
         
-        return ((fpow(4 , four) % mod)  * 1L * (fpow(5 , five) % mod) % mod);
+        long long store = fpow(((base % mod) * (base % mod)) % mod , power / 2);
+        if(power & 1) return ((store % mod) * (base % mod)) % mod;
+        return store % mod;
+    }
+    
+    int countGoodNumbers(long long n) {
+        long long even = fpow(5 , n / 2 + n % 2) % mod;
+        long long odd = fpow(4 , n / 2) % mod;
+        
+        long long ans = (even * odd) % mod;
+        return ans;
     }
 };
+
