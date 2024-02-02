bab96ef7d519651220c6a706e61bbcb2936b5f49
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-02 17:42:19 +0530

    Time: 4 ms (94.81%), Space: 12.8 MB (50.93%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    vector <vector <int>> ans;
-    
-    void solve(vector <vector <int>> &graph , vector <bool> &visited , int src , int dest , vector <int> &cur) {
+    void findPath(vector <vector <int>> &graph , vector <bool> &visited , int src , vector <int> &cur_path , 
+                  vector <vector <int>> &total_path) {
         
-        if(src == dest) {
-            cur.push_back(src);
-            ans.push_back(cur);
-            cur.pop_back();
-            
+        if(src == graph.size() - 1) {
+            cur_path.push_back(src);
+            total_path.push_back(cur_path);
+            cur_path.pop_back();
             return;
         }
-        
-        if(visited[src]) return;
+    
+        visited[src] = true;
+        cur_path.push_back(src);
         
         for(auto adj: graph[src]) {
-            cur.push_back(src);
-            visited[src] = true;
-            solve(graph , visited , adj , dest , cur);
-            visited[src] = false;
-            cur.pop_back();
+            findPath(graph , visited , adj , cur_path , total_path);
         }
         
+        visited[src] = false;
+        cur_path.pop_back();
         
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
+        vector <vector <int>> total_path;
+        vector <int> cur_path;
         vector <bool> visited(n);
-        vector <int> cur;
-        solve(graph , visited , 0 , n - 1 , cur);
         
-        return ans;
+        findPath(graph , visited , 0 , cur_path , total_path);
+        
+        return total_path;
     }
 };
