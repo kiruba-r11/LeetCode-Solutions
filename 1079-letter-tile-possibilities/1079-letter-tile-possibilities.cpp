14e4c8d67ed19a5637cfa64f38a4e162c85b6d60
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-02 18:31:16 +0530

    Time: 1689 ms (5.04%), Space: 18.2 MB (16.38%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    map <string , int> hash;
-    void solve(string &tiles , string &cur) {
+    set <string> s;
+    void numTile(string &tiles , string &cur , int n) {
+        if(n == 0) {
+            if(cur.size()) s.insert(cur);
+            return;
+        }
         
         for(int i = 0; i < tiles.size(); i++) {
             if(tiles[i] != '-') {
                 char temp = tiles[i];
                 tiles[i] = '-';
+                
+                // exclude
+                numTile(tiles , cur , n - 1);
+                
+                // make effort
                 cur.push_back(temp);
                 
-                solve(tiles , cur);
-                hash[cur]++;
+                // include
+                numTile(tiles , cur , n - 1);
                 
-                cur.pop_back();
+                // undo effort
                 tiles[i] = temp;
+                cur.pop_back();
             }
         }
+        
     }
-    
     int numTilePossibilities(string tiles) {
         string cur = "";
-        solve(tiles , cur);
-        return hash.size();
+        int n = tiles.size();
+        numTile(tiles , cur , n);
+        return s.size();
     }
 };
