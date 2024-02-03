ddc57f4aaba65571f75251bb34a0ca5ed01d3abd
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-03 10:01:13 +0530

    Time: 8 ms (61.39%), Space: 24.7 MB (13.29%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    void solve(string &s , int n , int &k , vector <char> &list , string &ans) {
+    void happyString(int n , string &cur , vector <string> &ans) {
         
         if(n == 0) {
-            k--;
-            if(k == 0) {
-                ans = s;
-            }
+            ans.push_back(cur);
             return;
         }
-        
-        int size = list.size();
-        for(int i = 0; i < size; i++) {
-            if(s.size() == 0 || (s.size() != 0 && list[i] != s.back())) {
-                s.push_back(list[i]);
-                solve(s , n - 1 , k , list , ans);
-                s.pop_back();
-            }  
+    
+        for(int i = 'a'; i <= 'c'; i++) {
+            if(cur.size() == 0 || cur.back() != i) {
+                cur.push_back(i);
+                happyString(n - 1 , cur , ans);
+                cur.pop_back();
+            }
         }
         
     }
+    
     string getHappyString(int n, int k) {
+        vector <string> ans;
+        string cur = "";
         
-        /*
+        happyString(n , cur , ans);
         
-        O(2^N) time and O(N) space (recursive stack) complexity
-        Idea -> Backtracking
-        
-        */
-        
-        string s = "" , ans = "";
-        vector <char> list = {'a' , 'b' , 'c'};
-        
-        solve(s , n , k , list , ans);
-        return ans;
+        return ans.size() < k ? "" : ans[k - 1];
     }
 };
