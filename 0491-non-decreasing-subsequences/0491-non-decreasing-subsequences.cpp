530a7e778d59472c8789b5b1239c7c586a7f97c7
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-03 17:31:09 +0530

    Time: 67 ms (53.72%), Space: 29.1 MB (43.63%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -2,29 +2,35 @@ class Solution {
 public:
     vector <vector <int>> ans;
     set <vector <int>> s;
-    void solve(vector <int> &nums , vector <int> &cur , int i) {
+    void subsequence(vector <int> &nums , vector <int> &cur , int start) {
         
-        if(i == nums.size()) {
-            if(cur.size() > 1 && s.count(cur) == 0) {
-                ans.push_back(cur);
+        if(cur.size() >= 2) {
+            if(s.count(cur) == 0) {
                 s.insert(cur);
+                ans.push_back(cur);
             }
-            return;
         }
-        
-        solve(nums , cur , i + 1);
-        
-        if(cur.size() == 0 || cur.back() <= nums[i]) {
-            cur.push_back(nums[i]);
-            solve(nums , cur , i + 1);
-            cur.pop_back();
+    
+        for(int i = start; i < nums.size(); i++) {
+            if(nums[i] != -101) {
+                if(cur.size() == 0 || cur.back() <= nums[i]) {
+                    int temp = nums[i];
+                    nums[i] = -101;
+                    cur.push_back(temp);
+                    
+                    subsequence(nums , cur , i + 1);
+                    
+                    cur.pop_back();
+                    nums[i] = temp;
+                }
+            }
         }
         
     }
     
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         vector <int> cur;
-        solve(nums , cur , 0);
+        subsequence(nums , cur , 0);
         return ans;
     }
 };
