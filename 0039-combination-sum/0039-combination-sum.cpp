fbba29652792d9b7a166615baa4923d0315da3e1
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-03 10:36:13 +0530

    Time: 7 ms (55.00%), Space: 16.1 MB (37.85%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    vector <vector <int>> ans;
-    
-    void solve(vector <int> &nums , vector <int> &cur , int target , int i) {
+    void combination(vector <int> &nums , int i , int &target , vector <int> &cur , 
+                     vector <vector <int>> &ans) {
+        
+        if(target < 0) return;
         if(target == 0) {
             ans.push_back(cur);
             return;
         }
-        if(target < 0 || i == nums.size()) return;
+        if(i == nums.size()) return;
         
-        solve(nums , cur , target , i + 1);
+        // don't choose and move
+        combination(nums , i + 1 , target , cur , ans);
         
+        // choose and move = choose and don't move (current) + don't choose and move (next)
+        
+        // choose and don't move 
         cur.push_back(nums[i]);
-        if(target >= nums[i]) solve(nums , cur , target - nums[i] , i);
+        target -= nums[i];
+        combination(nums , i , target , cur , ans);
         cur.pop_back();
+        target += nums[i];
     }
     
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector <vector <int>> ans;
         vector <int> cur;
-        solve(nums , cur , target , 0);
+        combination(candidates , 0 , target , cur , ans);
         return ans;
     }
 };
