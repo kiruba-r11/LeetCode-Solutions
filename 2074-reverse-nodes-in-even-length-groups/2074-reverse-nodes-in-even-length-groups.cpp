1bb2fb8ba70fa949b69b5b75b6571cc050fd00ed
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-05 09:49:02 +0530

    Time: 588 ms (89.88%), Space: 333.2 MB (34.38%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.cpp
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.cpp
@@ -10,53 +10,37 @@
  */
 class Solution {
 public:
-    ListNode* reverse(ListNode* start , ListNode* prev , ListNode* end) {
-        
-        ListNode* p = NULL;
-        ListNode* q = start;
-        ListNode* r = start->next;
-        
-        while(r != end) {
-            q->next = p;
-            p = q;
-            q = r;
-            r = r->next;
-        }
-
-        q->next = p;
-        start = q;
-        
-        return start;
+    ListNode* reverse(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* newHead = reverse(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
     }
     
     ListNode* reverseEvenLengthGroups(ListNode* head) {
-        ListNode* temp = head;
-        ListNode* dummy = new ListNode(-1);
-        ListNode* itr = dummy;
-        int cnt = 1;
-        while(temp) {
-            int cur = 1;
-            ListNode* start = temp;
-            ListNode* end = temp;
-            ListNode* prev = temp;
-            int num = 0;
-            while(cur <= cnt && temp) {
+        
+        ListNode* temp = head;  
+        ListNode* prev = NULL;
+        for(int k = 1; temp; k++) {
+            ListNode* before = prev;
+            int cnt = 0;
+            for(int i = 1; i <= k && temp; i++) {
                 prev = temp;
                 temp = temp->next;
-                cur++;
-                num++;
+                cnt++;
             }
-            end = temp;
-            if(num % 2 == 0) {
-                itr->next = reverse(start , prev , end);
-                itr = start;
-            } else {
-                itr->next = start;
-                itr = prev;
+            if(cnt % 2 == 0) {
+                prev->next = NULL;
+                ListNode* newHead = reverse(before->next);
+                if(before->next) before->next->next = temp;
+                prev = before->next;
+                before->next = newHead;
             }
-            cnt++;
         }
         
-        return dummy->next;
+        return head;
     }
 };
+
+    
