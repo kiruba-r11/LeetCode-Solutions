5da065f66f4f0c2dc20c295583773d8692d1c98a
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-08 18:53:23 +0530

    Time: 0 ms (100.00%), Space: 12.7 MB (23.29%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -12,90 +12,27 @@
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
-        
-        
-        /*
-        
-        O(N) time and space complexity
-        Idea -> BFS
-        
-        Edge Cases -> 
-            1. There cannot exist a right node for next level without having a left node.
-               
-               Example - 
-               
-                            |
-                    |               |
-                -       |       |       |
-                
-               Here (-) indicates a NULL
-               
-               
-            2. If there is a left node, and no right node in the next level, then the next nodes in 
-               the current level should not have both left and right nodes.
-               
-               Example - 
-               
-                            |
-                    |               |
-                |       -       |       |
-                
-               Here (-) indicates a NULL
-            
-            3. If there is no left and right node in the next level, then the next nodes in the current 
-               level should have both left and right nodes. 
-            
-               Example - 
-               
-                            |
-                    |               |
-                -       -       |       |
-                
-               Here (-) indicates a NULL
-               
-            4. Except last level, all the levels should be perfectly filled
-        
-        */
-        
-        
         queue <TreeNode*> q;
         q.push(root);
         
-        int cnt = 1;
-        
+        int empty = 0;
         while(!q.empty()) {
             int count = q.size();
-            bool miss = false;
-            for(int it = 0; it < count; it++) {
-                
+            for(int i = 0; i < count; i++) {
                 TreeNode* node = q.front();
                 q.pop();
                 
-                if(node == NULL) {
-                    if(q.empty() || q.front() == NULL) continue;
-                    return false;
-                }
-                
                 if(node->left) {
-                    if(miss) return false;
+                    if(empty == -1) return false;
                     q.push(node->left);
                 }
-                else {
-                    if(node->right) return false;
-                }
+                else empty = -1;
                 if(node->right) {
-                    if(miss) return false;
-                    q.push(node->right);
+                    if(empty == -1) return false;
+                    q.push(node->right); 
                 }
-                else {
-                    if(node->left) q.push(NULL);
-                    else miss = true;
-                }   
+                else empty = -1;
             }
-            if(!q.empty()) {
-                if(cnt != count) return false;
-            }
-            cnt *= 2;
         }
         
         return true;
