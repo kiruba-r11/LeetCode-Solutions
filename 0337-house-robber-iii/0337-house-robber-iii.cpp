ac8b68c06f948299294461127db023d4b5385dce
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-10 11:51:15 +0530

    Time: 4 ms (95.75%), Space: 20.5 MB (97.46%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -11,22 +11,42 @@
  */
 class Solution {
 public:
+    
+    // Approach
+    // current node is robbed -> child should not be robbed
+    // current node is not robbed -> max(child robbed, child did not rob)
+    
     pair <int , int> house(TreeNode* root) {
         if(!root) return {0 , 0};
         if(!root->left && !root->right) return {root->val , 0};
         
-        // include , don't include
+        // rob , notrob
         pair <int , int> left = house(root->left);
         pair <int , int> right = house(root->right);
         
-        // include -> use don't include answers
-        int include = left.second + right.second + root->val;
+        int rob = left.second + right.second + root->val;
+        int notrob = max(left.first , left.second) + max(right.first , right.second);
         
-        // exclude -> use include answers
-        int exclude = left.first + right.first;
-        
-        return {max(include , exclude) , exclude};
+        return {rob , notrob};
     }
+    
+//     pair <int , int> house(TreeNode* root) {
+//         if(!root) return {0 , 0};
+//         if(!root->left && !root->right) return {root->val , 0};
+        
+//         // include , don't include
+//         pair <int , int> left = house(root->left);
+//         pair <int , int> right = house(root->right);
+        
+//         // include -> use don't include answers
+//         int include = left.second + right.second + root->val;
+        
+//         // exclude -> use include answers
+//         int exclude = left.first + right.first;
+        
+//         return {max(include , exclude) , exclude};
+//     }
+    
     int rob(TreeNode* root) {
         pair <int , int> ans = house(root);
         return max(ans.first , ans.second);
