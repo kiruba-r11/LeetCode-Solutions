e818ee87df3de0032a366e47b30ffc06ed114824
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-11 09:09:08 +0530

    Time: 15 ms (58.46%), Space: 26.1 MB (64.80%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,30 +13,20 @@ class Solution {
 public:
     unordered_map <int , int> hash;
     
-    bool isinrange(vector <int> &in , int start , int end , int elem) {
-        for(int i = start; i <= end; i++) {
-            if(in[i] == elem) return true;
-        }
-        return false;
-    }
-    
     TreeNode* build(vector <int> &pre , vector <int> &in , int &i , int start , int end) {
         
         if(i == pre.size()) return NULL;
-        
-        bool range = isinrange(in , start , end , pre[i]);
-        if(range) {
-            TreeNode* node = new TreeNode(pre[i]);
-            int idx = hash[pre[i]];
-            i++;
-            node->left = build(pre , in , i , start , idx - 1);
-            node->right = build(pre , in , i , idx + 1 , end);
-            
-            return node;
-        }
-        
-        return NULL;
+        if(start > end) return NULL;
+    
+        TreeNode* node = new TreeNode(pre[i]);
+        int idx = hash[pre[i]];
+        i++;
+        node->left = build(pre , in , i , start , idx - 1);
+        node->right = build(pre , in , i , idx + 1 , end);
+
+        return node;    
     }
+    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = inorder.size();
         for(int i = 0; i < n; i++) {
