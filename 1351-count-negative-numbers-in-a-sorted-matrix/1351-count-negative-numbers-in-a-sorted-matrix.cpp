063a54438c810a7ba9a830215c9660b36fdd0c04
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-11 15:41:22 +0530

    Time: 8 ms (91.56%), Space: 13 MB (6.18%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    int bs(vector <int> &nums) {
-        int n = nums.size();
-        int low = 0 , high = n - 1;
-        int ans = n;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(nums[mid] <= -1) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        
-        return ans;
-    }
     int countNegatives(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int ans = 0;
-        for(int i = 0; i < n; i++) {
-            int pos = bs(grid[i]);
-            ans += m - pos;
+        int i = 0 , j = m - 1;
+        while(i <= n - 1 && j >= 0) {
+            if(grid[i][j] < 0) {
+                ans += n - i;
+                j--;
+            } else {
+                i++;
+            }
         }
         
         return ans;
