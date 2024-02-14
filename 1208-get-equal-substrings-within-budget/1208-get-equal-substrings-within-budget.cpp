9de95ca233c5a209b552743160e52e8fbb9553c1
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-14 18:15:03 +0530

    Time: 12 ms (10.09%), Space: 8.6 MB (43.97%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
-    int equalSubstring(string s, string t, int maxCost) {
+    bool condition(string &s , string &t , int maxCost , int k) {
+    
         int n = s.size();
-        int i = 0 , j = 0 , ans = 0;
+        int i = 0 , j = 0;
+        int cost = 0;
         
         while(j < n) {
-            int cost = abs(s[j] - t[j]);
-            if(cost <= maxCost) {
-                ans = max(ans , j - i + 1);
-                maxCost -= cost;
-                j++;
-            } else {
-                while(maxCost < cost) {
-                    maxCost += abs(s[i] - t[i]);
-                    i++;
+            int window = j - i + 1;
+            if(window <= k) {
+                cost += abs(s[j] - t[j]);
+                
+                if(window == k) {
+                    if(cost <= maxCost) return true;
                 }
+                
+                j++;
+                
+            } else {
+                cost -= abs(s[i] - t[i]);
+                i++;
+            }
+        }
+        
+        return false;
+    }
+    
+    int equalSubstring(string s, string t, int maxCost) {
+        int n = s.size();
+        int low = 0 , high = n;
+        int ans = low;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(condition(s , t , maxCost , mid)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         
