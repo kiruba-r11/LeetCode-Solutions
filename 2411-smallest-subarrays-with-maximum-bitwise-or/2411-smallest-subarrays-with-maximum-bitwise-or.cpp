8eaa40a57e15a56e5b93ee234a4854ec932406d1
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-15 18:25:58 +0530

    Time: 194 ms (30.30%), Space: 151.8 MB (11.11%) - LeetHub

--- 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -1,24 +1,57 @@
 class Solution {
 public:
     vector<int> smallestSubarrays(vector<int>& nums) {
-        
         int n = nums.size();
-        vector <int> store(30 , 0);
+        int max_or = 0;
+        
+        vector <vector <int>> bits(n , vector <int> (32));
+        for(int i = n - 1; i >= 0; i--) {
+            max_or |= nums[i];
+            for(int k = 0; k < 32; k++) {
+                bits[i][k] = max_or & (1 << k);
+            }
+        }
+        
+        vector <long long> req(32 , 0);
         vector <int> ans(n);
         
+        int start = 0 , cnt = 0;
+        
         for(int i = n - 1; i >= 0; i--) {
-            
-            int idx = -1;
-            for(int bit = 0; bit < 30; bit++) {
-                if((1 << bit) & nums[i]) {
-                    store[bit] = i;
-                }
-                idx = max(idx , store[bit]);
+            if(nums[i] == 0) {
+                cnt++;
+                ans[i] = 1;
             }
-            
-            ans[i] = (idx - i + 1) <= 0 ? 1 : (idx - i + 1);
+            else break;
+        }
+        
+        for(int i = 0 , start = 0; start < n - cnt;) {
+            bool check = true;
+            for(int k = 0; k < 32; k++) {
+                if(req[k] < bits[start][k]) {
+                    check = false;
+                    break;
+                }
+            }
+            if(check) {
+                ans[start] = i - start;
+                
+                for(int k = 0; k < 32; k++) {
+                    req[k] -= nums[start] & (1 << k);
+                }
+                start++;
+            } else {
+                if(i < n) {
+                    for(int k = 0; k < 32; k++) {
+                        req[k] += nums[i] & (1 << k);
+                    }
+                    i++;
+                }
+            }
         }
         
         return ans;
     }
 };
+
+
