acc8683c1c88873eabdb507183c5845c1ff3d6da
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-18 22:12:10 +0530

    Time: 10 ms (81.54%), Space: 15.1 MB (13.85%) - LeetHub

--- 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
@@ -9,16 +9,16 @@ public:
         int prefix = 0;
         int min_val = 0;
         int diff = INT_MAX;
+        
         for(int i = 0; i < n; i++) {
             int cnt = n - i;
+            
             int val1 = (target - prefix) / cnt;
             int val2 = (target - prefix) / cnt + ((target - prefix) % cnt != 0);
 
             int c1 = abs(target - (prefix + val1 * cnt));
             int c2 = abs(target - (prefix + val2 * cnt));
-            // cout << prefix << endl;
-            // cout << val1 << " val " << val2 << endl;
-            // cout << c1 << " c " << c2 << endl;
+            
             if(val1 <= arr[i] && val2 <= arr[i]) {
                 if(val1 >= min_val && val2 >= min_val) {
                     if(diff > c1 && diff > c2) {
@@ -45,7 +45,7 @@ public:
                     if(diff > c2) diff = c2 , ans = val2;
                 }
             } 
-            // cout << ans << " ans : diff " << diff << endl;
+
             prefix += arr[i]; 
             min_val = arr[i];
         }
