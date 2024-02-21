a0825f1f5fc464de16c199282b7c43e213e17cc8
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-21 09:34:50 +0530

    Time: 4 ms (55.46%), Space: 7.9 MB (40.33%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -2,34 +2,18 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         vector <int> pending(26);
-        vector <int> entry(26);
         vector <int> ans;
         
         int n = s.size();
         int lastentry = -1;
         
-        for(int i = 0; i < n; i++) {
-            pending[s[i] - 'a']++;
-        }
+        for(int i = 0; i < n; i++) pending[s[i] - 'a'] = i;
         
-        for(int i = 0; i < n; i++) {
-            entry[s[i] - 'a']++;
-            pending[s[i] - 'a']--;
+        for(int i = 0 , j = 0; i < n; i++) {
             
-            bool nope = false;
+            j = max(j , pending[s[i] - 'a']);
             
-            for(int k = 0; k < 26; k++) {
-                if(entry[k] != 0) {
-                    if(pending[k] != 0) {
-                        nope = true;
-                        break;
-                    }
-                }
-            }
-            
-            if(!nope) {
-                entry.clear();
-                entry.resize(26);
+            if(i == j) {
                 ans.push_back(i - lastentry);
                 lastentry = i;
             }
