3bebd4689b27c2bf9983bc5507d917bdb02cae57
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-21 17:41:43 +0530

    Time: 131 ms (57.64%), Space: 100.9 MB (29.91%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int bs(vector <int> &nums , int low , int high , int key) {
-    
-        int ans = low;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(key <= nums[mid]) {
-                ans = mid;
-                low = mid + 1;
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size() , n2 = nums2.size();
+        int i = 0 , j = 0;
+        int ans = 0;
+        
+        while(i < n1 && j < n2) {
+            if(nums1[i] <= nums2[j]) {
+                if(i <= j) ans = max(ans , j - i);
+                j++;
             } else {
-                high = mid - 1;
+                i++;
             }
         }
         
-        return ans;
-    }
-    
-    
-    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size() , m = nums2.size();
-        int ans = 0;
-        for(int i = 0; i < n; i++) {
-            int j = bs(nums2 , i , m - 1 , nums1[i]);
-            ans = max(ans , j - i);
-        }
         return ans;
     }
 };
