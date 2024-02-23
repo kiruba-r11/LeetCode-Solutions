0b81d4d58c9bb1d19be7fd7a1e036497905a4e59
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-23 17:00:11 +0530

    Time: 16 ms (67.70%), Space: 26.4 MB (34.61%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        // Multiple Approaches:
+        // O(N) time and O(N) space -> using prefix and suffix array (no division)
+        // O(N) time and O(1) space (two pass) -> using prefix and suffix variable in ans array (no division)
+        // O(N) time and O(1) space (one pass) -> combine above approach in single loop (no division)
+        
         int n = nums.size();
         vector <int> ans(n , 1);
         
         int pre = 1;
+        int suf = 1;
+        
         for(int i = 0; i < n; i++) {
             ans[i] = ans[i] * pre;
             pre = pre * nums[i];
-        }
-        
-        int suf = 1;
-        for(int i = n - 1; i >= 0; i--) {
-            ans[i] = ans[i] * suf;
-            suf = suf * nums[i];
+            ans[n - i - 1] = ans[n - i - 1] * suf;
+            suf = suf * nums[n - i - 1];
         }
         
         return ans;
