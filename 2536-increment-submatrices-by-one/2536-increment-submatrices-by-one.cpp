5de5fee743f57fd6564029d940529a7e9bb4b36d
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-24 18:50:16 +0530

    Time: 154 ms (96.05%), Space: 85.4 MB (61.02%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+    bool inbounds(int size , int row , int col) {
+        if(row >= 0 && row < size && col >= 0 && col < size) return true;
+        return false;
+    }
+    
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        int m = queries.size();
         vector <vector <int>> ans(n , vector <int> (n));
         
-        int m = queries.size();
         for(int i = 0; i < m; i++) {
-            int sr = queries[i][0];
-            int sc = queries[i][1];
-            int er = queries[i][2];
-            int ec = queries[i][3];
+            int si = queries[i][0];
+            int sj = queries[i][1];
+            int ei = queries[i][2];
+            int ej = queries[i][3];
             
-            ans[sr][sc]++;
-            
-            if(er + 1 < n && ec + 1 < n) ans[er + 1][ec + 1]++;
-            if(er + 1 < n) ans[er + 1][sc]--;
-            if(ec + 1 < n) ans[sr][ec + 1]--;
+            if(inbounds(n , si , sj)) ans[si][sj] += 1;
+            if(inbounds(n , ei + 1 , ej + 1)) ans[ei + 1][ej + 1] += 1;
+            if(inbounds(n , ei + 1 , sj)) ans[ei + 1][sj] -= 1;
+            if(inbounds(n , si , ej + 1)) ans[si][ej + 1] -= 1;
         }
-
         
         for(int i = 0; i < n; i++) {
             for(int j = 1; j < n; j++) {
@@ -24,12 +27,19 @@ public:
             }
         }
         
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                ans[i][j] += ans[i - 1][j];
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j < n; j++) {
+                ans[j][i] += ans[j - 1][i];
             }
         }
         
         return ans;
     }
 };
+
+// Sweep Line for 2D array
+// Given ((si , sj) , (ei , ej)) 
+// -> grid[si][sj] += 1
+// -> grid[ei + 1][ej + 1] += 1
+// -> grid[ei + 1][sj] -= 1
+// -> grid[si][ej + 1] -= 1
