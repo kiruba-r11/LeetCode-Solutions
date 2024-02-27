b7e17035ba984bc8bba8bb287b60a0876a90db15
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-27 20:25:16 +0530

    Time: 260 ms (45.60%), Space: 12.3 MB (25.18%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -10,6 +10,7 @@ public:
         }
         
         for(int i = 0; i < 4; i++) {
+            // pruning condition
             if(sq[i] + matchsticks[idx] <= sum) {
                 sq[i] += matchsticks[idx];
                 if(dfs(matchsticks , idx + 1 , sq , sum)) return true;
@@ -26,11 +27,13 @@ public:
         int sum = 0;
         int n = matchsticks.size();
         
+        // pruning
         sort(matchsticks.rbegin() , matchsticks.rend());
         for(int i = 0; i < n; i++) sum += matchsticks[i];
         
         if(sum % 4) return false;
         if(matchsticks[0] > sum / 4) return false;
+        // ---
         
         return dfs(matchsticks , 0 , sq , sum / 4);
     }
