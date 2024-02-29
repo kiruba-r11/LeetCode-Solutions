1cd205923e786d228ab5069526efdc4c63d98459
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-02-29 09:24:03 +0530

    Time: 284 ms (39.74%), Space: 72.5 MB (40.60%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    int maxUniqueSplit(string s) {
-        
-        /*
-        
-        O(Nlog(N)) time and O(26) space complexity
-        Idea -> Backtracking or Bit Manipulations
-        
-        */
-        
-        int n = s.size() , start = 0 , end = (1 << n) - 1 , ans = 0;
-        bool check = true;
-        
-        string temp = "";
-        set <string> dup; 
-
-        for(int i = start; i <= end; i++) {
-            temp = "";
-            check = true;
-            for(int j = n - 1 , k = 0; j >= 0; j-- , k++) {
-                temp += s[k];
-                if(i & (1 << j)) {
-                    if(dup.count(temp) == 0) {
-                        dup.insert(temp);
-                        temp = "";
-                    } else {
-                        check = false;
-                        break;
-                    }
-                }
-            }
-            if(check) {
-                ans = max(ans , (int)dup.size());
-            }
-            dup.clear();
+    map <string , bool> hash;
+    int ans = 0;
+    
+    void solve(string &s , int idx) {
+        if(idx == s.size()) {
+            ans = max(ans , (int)hash.size());
+            return;
         }
         
+        string prefix = "";
+        for(int i = idx; i < s.size(); i++) {
+            prefix += s[i];
+            if(hash.find(prefix) == hash.end()) {
+                hash[prefix] = true;
+                solve(s , i + 1);
+                hash.erase(prefix);
+            }
+        }
+    }
+    
+    int maxUniqueSplit(string s) {
+        solve(s , 0);
         return ans;
     }
 };
