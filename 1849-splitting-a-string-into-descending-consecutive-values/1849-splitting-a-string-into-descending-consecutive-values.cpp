8efac9a71e7085aff3a271b63cdc324857985ef9
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-03-01 11:40:31 +0530

    Time: 3 ms (46.54%), Space: 7.3 MB (52.07%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.cpp
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool valid(long long &cur , long long &prev , int i , int n) {
-        cout << cur << " " << prev << endl;
         if(i == n - 1 && prev == -1) return false;
         if(prev == -1) return true;
         
@@ -9,12 +8,8 @@ public:
         return false;
     }
     
-    bool solve(string &t , string &s , long long &prev , int start) {
-        if(start == s.size()) {
-            // if(to_string(prev) == t) return false;
-            // if(prev == -1) return false;
-            return true;
-        }
+    bool solve(string &s , long long &prev , int start) {
+        if(start == s.size()) return true;
         
         long long prefix = 0;
         int len = s.size() / 2 + 1;
@@ -26,7 +21,7 @@ public:
             if(valid(prefix , prev , i , s.size())) {
                 int temp = prev;
                 prev = prefix;
-                if(solve(t , s , prev , i + 1)) return true;
+                if(solve(s , prev , i + 1)) return true;
                 prev = temp;
             }
         }
@@ -36,21 +31,6 @@ public:
     
     bool splitString(string s) {
         long long prev = -1;
-        string temp = "";
-        int idx = -1;
-        
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '0') continue;
-            else {
-                idx = i;
-                break;
-            }
-        }
-        
-        for(int i = idx; i < s.size(); i++) {
-            temp += s[i];
-        }
-        cout << temp << endl;
-        return solve(temp , s , prev , 0);
+        return solve(s , prev , 0);
     }
 };
