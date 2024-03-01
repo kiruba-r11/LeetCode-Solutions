33ab9b2b2b2df79eda4a69c9544a3d1b0931f5bb
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-03-01 11:39:09 +0530

    Time: 4 ms (30.41%), Space: 7.7 MB (47.93%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.cpp
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.cpp
@@ -1,45 +1,32 @@
 class Solution {
 public:
-    bool valid(string &cur , string &prev) {
-        if(prev == "-") return true;
-        int clen = cur.size();
-        int plen = prev.size();
+    bool valid(long long &cur , long long &prev , int i , int n) {
+        cout << cur << " " << prev << endl;
+        if(i == n - 1 && prev == -1) return false;
+        if(prev == -1) return true;
         
-        int clzero = 0;
-        int plzero = 0;
-        
-        for(int i = 0; i < clen; i++) {
-            if(cur[i] == '0') clzero++;
-            else break;
-        }
-        
-        for(int i = 0; i < plen; i++) {
-            if(prev[i] == '0') plzero++;
-            else break;
-        }
-        
-        if(abs((clen - clzero) - (plen - plzero)) >= 2) return false;
-            
-        long long c = stoll(cur);
-        long long p = stoll(prev);
-        
-        if(c < p && (p - c) == 1) return true;
+        if(cur < prev && (prev - cur) == 1) return true;
         return false;
     }
     
-    bool solve(string &s , string &prev , int start) {
+    bool solve(string &t , string &s , long long &prev , int start) {
         if(start == s.size()) {
-            if(prev == s) return false;
+            // if(to_string(prev) == t) return false;
+            // if(prev == -1) return false;
             return true;
         }
         
-        string prefix = "";
+        long long prefix = 0;
+        int len = s.size() / 2 + 1;
+        
         for(int i = start; i < s.size(); i++) {
-            prefix += s[i];
-            if(valid(prefix , prev)) {
-                string temp = prev;
+            prefix = prefix * 10LL + (s[i] - '0');
+            if(prefix > pow(10 , len)) break;
+            
+            if(valid(prefix , prev , i , s.size())) {
+                int temp = prev;
                 prev = prefix;
-                if(solve(s , prev , i + 1)) return true;
+                if(solve(t , s , prev , i + 1)) return true;
                 prev = temp;
             }
         }
@@ -48,7 +35,22 @@ public:
     }
     
     bool splitString(string s) {
-        string prev = "-";
-        return solve(s , prev , 0);
+        long long prev = -1;
+        string temp = "";
+        int idx = -1;
+        
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '0') continue;
+            else {
+                idx = i;
+                break;
+            }
+        }
+        
+        for(int i = idx; i < s.size(); i++) {
+            temp += s[i];
+        }
+        cout << temp << endl;
+        return solve(temp , s , prev , 0);
     }
 };
