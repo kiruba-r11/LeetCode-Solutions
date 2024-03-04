1d1c48a62918a598f0417228354359e6d4ffd294
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-03-04 19:44:19 +0530

    Time: 4 ms (41.11%), Space: 9.6 MB (28.41%) - LeetHub

--- 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.cpp
+++ 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.cpp
@@ -11,56 +11,35 @@ public:
         return false;
     }
     
-    set <vector <int>> s;
+    unordered_map <int , int> count;
+    unordered_map <int , unordered_set <int>> store;
     int ans = 0;
     
-    void solve(int n , map <int , int> &nums , unordered_map <int , unordered_map <int , bool>> &store , vector <int> &cur) {
+    void dfs(int element , int left) {
+        count[element]--;
         
-        if(cur.size() == n) {
-            ans++;
-            return;
+        if(left == 0) ans++;
+        for(auto i: store[element]) {
+            if(count[i] != 0) dfs(i , left - 1);
         }
         
-        for(auto it: nums) {
-            if(nums[it.first] != 0) {
-                if(cur.size() == 0) {
-                    cur.push_back(it.first);
-                    nums[it.first]--;
-                    solve(n , nums , store , cur);
-                    nums[it.first]++;
-                    cur.pop_back();
-                } else {
-                    if(store[cur.back()].find(it.first) != store[cur.back()].end()) {
-                        cur.push_back(it.first);
-                        nums[it.first]--;
-                        solve(n , nums , store , cur);
-                        nums[it.first]++;
-                        cur.pop_back();
-                    }
-                }
-            }
-        }
+        count[element]++;
     }
     
     int numSquarefulPerms(vector<int>& nums) {
         int n = nums.size();
-        map <int , int> arr;
-        unordered_map <int , unordered_map <int , bool>> store;
-        sort(nums.begin() , nums.end());
         for(int i = 0; i < n; i++) {
+            count[nums[i]]++;
             for(int j = i + 1; j < n; j++) {
                 long long pair = nums[i] + nums[j];
                 if(perfect_square(pair)) {
-                    store[nums[i]][nums[j]] = true;
-                    store[nums[j]][nums[i]] = true;
+                    store[nums[i]].insert(nums[j]);
+                    store[nums[j]].insert(nums[i]);
                 }
             }
         }
         
-        for(int i = 0; i < n; i++) arr[nums[i]]++;
-        
-        vector <int> cur;
-        solve(n , arr , store , cur);
+        for(auto i: count) dfs(i.first , n - 1);
         return ans;
     }
 };
