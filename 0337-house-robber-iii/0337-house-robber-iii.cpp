72fc31e28905ed0fa9d206602c03eebb6c45d5eb
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-03-06 18:18:05 +0530

    Time: 0 ms (100.00%), Space: 20.6 MB (94.86%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -11,44 +11,25 @@
  */
 class Solution {
 public:
+    pair <int , int> solve(TreeNode* root) {
     
-    // Approach
-    // current node is robbed -> child should not be robbed
-    // current node is not robbed -> max(child robbed, child did not rob)
-    
-    pair <int , int> house(TreeNode* root) {
         if(!root) return {0 , 0};
         if(!root->left && !root->right) return {root->val , 0};
         
-        // rob , notrob
-        pair <int , int> left = house(root->left);
-        pair <int , int> right = house(root->right);
+        pair <int , int> left = solve(root->left);
+        pair <int , int> right = solve(root->right);
         
-        int rob = left.second + right.second + root->val;
-        int notrob = max(left.first , left.second) + max(right.first , right.second);
+        // rob this house
+        int rob = root->val + left.second + right.second;
+            
+        // don't rob this house
+        int dont_rob = max(left.first , left.second) + max(right.first , right.second);
         
-        return {rob , notrob};
+        return {rob , dont_rob};
     }
     
-//     pair <int , int> house(TreeNode* root) {
-//         if(!root) return {0 , 0};
-//         if(!root->left && !root->right) return {root->val , 0};
-        
-//         // include , don't include
-//         pair <int , int> left = house(root->left);
-//         pair <int , int> right = house(root->right);
-        
-//         // include -> use don't include answers
-//         int include = left.second + right.second + root->val;
-        
-//         // exclude -> use include answers
-//         int exclude = left.first + right.first;
-        
-//         return {max(include , exclude) , exclude};
-//     }
-    
     int rob(TreeNode* root) {
-        pair <int , int> ans = house(root);
+        pair <int , int> ans = solve(root);
         return max(ans.first , ans.second);
     }
 };
