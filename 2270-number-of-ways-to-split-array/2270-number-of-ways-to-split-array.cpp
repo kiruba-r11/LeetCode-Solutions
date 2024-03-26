21893c4c60c673e14b4064b2a45fb8caf0e5afe4
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-03-26 20:42:43 +0530

    Time: 116 ms (42.49%), Space: 87.9 MB (82.97%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cpp
@@ -2,23 +2,30 @@ class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
         int n = nums.size();
-        vector <long long> prefix(n);
-        vector <long long> suffix(n);
+        // vector <long long> prefix(n);
+        // vector <long long> suffix(n);
+        long long total = 0;
+        long long prefix = 0;
         
-        prefix[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
-        }
+//         prefix[0] = nums[0];
+//         for(int i = 1; i < n; i++) {
+//             prefix[i] = prefix[i - 1] + nums[i];
+//         }
         
-        suffix[n - 1] = nums[n - 1];
-        for(int i = n - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] + nums[i];
-        }
+//         suffix[n - 1] = nums[n - 1];
+//         for(int i = n - 2; i >= 0; i--) {
+//             suffix[i] = suffix[i + 1] + nums[i];
+//         }
+        
+        for(int i = 0; i < n; i++) total += nums[i];
         
         int cnt = 0;
         for(int i = 0; i < n - 1; i++) {
-            long long first = prefix[i];
-            long long last = suffix[i + 1];
+            // long long first = prefix[i];
+            // long long last = suffix[i + 1];
+            prefix += nums[i];
+            long long first = prefix;
+            long long last = total - prefix;
             if(first >= last) cnt++;
         }
         
