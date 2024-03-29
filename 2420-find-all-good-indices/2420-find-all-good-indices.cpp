24b5ff80bd7f87a017be8eb9faa0ff9a9b53025f
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-03-29 23:03:07 +0530

    Time: 118 ms (10.95%), Space: 97 MB (8.76%) - LeetHub

--- 2420-find-all-good-indices/2420-find-all-good-indices.cpp
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -2,23 +2,42 @@ class Solution {
 public:
     vector<int> goodIndices(vector<int>& nums, int k) {
         int n = nums.size();
-        vector <int> left(n , 1) , right(n , 1) , ans;
+        vector <pair <int , int>> prefix(n);
+        vector <pair <int , int>> suffix(n);
         
+        prefix[0] = {1 , -1};
         for(int i = 1; i < n; i++) {
-            if(nums[i] <= nums[i - 1]) {
-                left[i] = left[i - 1] + 1;
+            if(nums[i - 1] >= nums[i]) {
+                prefix[i] = prefix[i - 1];
+            } else {
+                prefix[i] = {0 , i};
             }
         }
         
+        suffix[n - 1] = {1 , n};
         for(int i = n - 2; i >= 0; i--) {
             if(nums[i] <= nums[i + 1]) {
-                right[i] = right[i + 1] + 1;
+                suffix[i] = suffix[i + 1];
+            } else {
+                suffix[i] = {0 , i};
             }
         }
         
+        vector <int> ans;
         for(int i = k; i < n - k; i++) {
-            if(left[i - 1] >= k && right[i + 1] >= k) {
+            int leftval = prefix[i - 1].first;
+            int leftidx = prefix[i - 1].second;
+            int rightval = suffix[i + 1].first;
+            int rightidx = suffix[i + 1].second;
+            
+            if(leftval == 1 && rightval == 1) {
                 ans.push_back(i);
+            } else if(leftval == 1) {
+                if(rightidx >= i + k) ans.push_back(i);
+            } else if(rightval == 1) {
+                if(leftidx <= i - k) ans.push_back(i);
+            } else {
+                if(leftidx <= i - k && rightidx >= i + k) ans.push_back(i);
             }
         }
         
