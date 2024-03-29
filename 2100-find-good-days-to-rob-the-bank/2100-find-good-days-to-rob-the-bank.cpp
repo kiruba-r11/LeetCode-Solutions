e39f9b06738c22812a72591d5de538b5f1f1f7bd
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-03-29 23:40:57 +0530

    Time: 100 ms (21.33%), Space: 92.4 MB (12.94%) - LeetHub

---
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> goodDaysToRobBank(vector<int>& nums, int time) {
+        // Sorted Prefix Sum pattern
+        int n = nums.size();
+        vector <int> prefix(n , 1);
+        vector <int> suffix(n , 1);
+        
+        for(int i = 1; i < n; i++) {
+            if(nums[i - 1] >= nums[i]) {
+                prefix[i] = prefix[i - 1] + 1;
+            }
+        }
+        
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] <= nums[i + 1]) {
+                suffix[i] = suffix[i + 1] + 1;
+            }
+        }
+        
+        vector <int> ans;
+        for(int i = time; i < n - time; i++) {
+            if(time == 0) ans.push_back(i);
+            else if(prefix[i] >= time + 1 && suffix[i] >= time + 1) ans.push_back(i);
+        }
+        
+        return ans;
+    }
+};
