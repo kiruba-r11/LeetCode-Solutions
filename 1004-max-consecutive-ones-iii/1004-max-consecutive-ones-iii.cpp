9b92de03f544b39522e28720bf96aa0ef177d06f
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-04-07 11:25:31 +0530

    Time: 39 ms (69.70%), Space: 57.9 MB (49.08%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    bool slide(vector <int> &nums , int allow , int k) {
+    int longestOnes(vector<int>& nums, int k) {
+        int i = 0 , j = 0 , ans = 0 , flips = 0;
         int n = nums.size();
-        int i = 0 , j = 0;
-        
         while(j < n) {
-            int window = j - i + 1;
-            if(window <= k) {
-                if(nums[j] == 0) allow--;
-                if(window == k) {
-                    if(allow >= 0) return true;
-                }
-                j++;
-            } else {
-                if(nums[i] == 0) allow++;
+            if(nums[j] == 0) flips++;
+            while(flips > k) {
+                if(nums[i] == 0) flips--;
                 i++;
             }
+            if(flips <= k) ans = max(ans , j - i + 1);
+            j++;
         }
         
-        return false;
-    }
-    
-    int longestOnes(vector<int>& nums, int k) {
-        int n = nums.size();
-        int low = 0 , high = n;
-        int ans = low;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            bool check = slide(nums , k , mid);
-            if(check) {
-                ans = mid;
-                low = mid + 1; // increases the size
-            } else {
-                high = mid - 1; // decreases the size
-            }
-        }
         return ans;
     }
 };
