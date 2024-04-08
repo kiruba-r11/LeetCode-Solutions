2d6858f86c8caddab4cd055d8f69a62fe9990333
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-04-08 09:49:21 +0530

    Time: 36 ms (8.03%), Space: 30.6 MB (57.98%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,43 +1,22 @@
 class Solution {
 public:
-    bool condition(vector <int> &nums , int target , int k) {    
-        
+    int minSubArrayLen(int target, vector<int>& nums) {
         int n = nums.size();
         int i = 0 , j = 0;
         int sum = 0;
+        int ans = n + 1;
         
         while(j < n) {
-            int window = j - i + 1;
-            if(window <= k) {
-                sum += nums[j];
-                
-                if(window == k) {
-                    if(sum >= target) return true;
-                }
-                
-                j++;
-            } else {
+            sum += nums[j];
+            while(sum >= target) {
+                if(sum - nums[i] < target) break;
                 sum -= nums[i];
                 i++;
             }
+            if(sum >= target) ans = min(ans , j - i + 1);
+            j++;
         }
         
-        return false;
-    }
-    
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int low = 0 , high = nums.size();
-        int ans = 0;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(condition(nums , target , mid)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        
-        return ans;
+        return ans == n + 1 ? 0 : ans;
     }
 };
