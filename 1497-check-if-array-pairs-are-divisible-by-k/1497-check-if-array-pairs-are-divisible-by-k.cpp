4b871d93cb566099e04c77cbad11255496920800
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-04-15 09:45:04 +0530

    Time: 170 ms (17.77%), Space: 73.2 MB (73.55%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -5,7 +5,7 @@ public:
         map <int , int> hash;
         
         for(int i = 0; i < n; i++) {
-            if((k + arr[i] % k) % k != 0) hash[(k + arr[i] % k) % k]++;
+            hash[(k + arr[i] % k) % k]++;
         }
         
         for(auto i: hash) {
@@ -13,6 +13,8 @@ public:
             int count = i.second;
             int pairelem = k - elem;
             int paircount = hash[pairelem];
+            
+            if(elem == 0 || pairelem == 0) continue;
             
             if(elem == pairelem) {
                 if(count & 1) return false;
