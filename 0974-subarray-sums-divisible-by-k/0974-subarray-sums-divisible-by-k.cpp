c6f3a683bd6b84daa3ce2bfdd8f5a2f3fb7e86fb
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-04-16 09:44:39 +0530

    Time: 64 ms (13.16%), Space: 34.2 MB (79.69%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        map <int , int> hash;
-        hash[0] = 1;
+        // Premise
+        // 1. sum(nums[i1..i2]) % k == x
+        // 2. sum(nums[i1..i3]) % k == x
+        // Then, sum(nums[i2+1..i3]) % k == 0 -> len = i3 - i2. Maximise this len
+        
+        int n = nums.size();
         int sum = 0;
-        int count = 0;
-        for(int i = 0; i < nums.size(); i++) {
+        int ans = 0;
+        map <int , int> hash;
+        hash[sum] = 1;
+        
+        for(int i = 0; i < n; i++) {
             sum += nums[i];
-            int rem = (sum % k + k) % k;
-            if(hash[rem] != 0) {
-                count += hash[rem];
-            }
-            hash[rem]++;
+            ans += hash[(k + sum % k) % k];
+            hash[(k + sum % k) % k]++;
         }
-        return count;
+        
+        return ans;
     }
 };
