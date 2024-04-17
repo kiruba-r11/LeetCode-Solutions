615626d6bb919e81441f9e1494277fd6e86c1c96
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-04-17 10:17:41 +0530

    Time: 151 ms (5.34%), Space: 110.5 MB (36.13%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -4,16 +4,16 @@ public:
         int n = arr.size();
         int sum = 0;
         int m = 1e9 + 7;
-        long long ans = 0;
+        int ans = 0;
         map <int , int> hash;
         hash[sum] = 1;
         
         for(int i = 0; i < n; i++) {
             sum += arr[i];
-            ans += hash[1 - sum % 2];
+            ans = (ans % m + hash[1 - sum % 2] % m) % m;
             hash[sum % 2]++;
         }
         
-        return ans % m;
+        return ans;
     }
 };
