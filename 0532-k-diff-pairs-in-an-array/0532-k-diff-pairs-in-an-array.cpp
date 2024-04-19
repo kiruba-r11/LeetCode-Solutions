a79a4bc81e2aaa301e984e13925b8b213b7d8c8d
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-04-19 09:23:21 +0530

    Time: 23 ms (17.03%), Space: 17.6 MB (11.95%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    int bs(vector <int> &nums , int low , int high , int val , int key) {
-        int ans = -1;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(val + key <= nums[mid]) {
-                if(val + key == nums[mid]) ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }   
-        return ans;
-    }
-    
     int findPairs(vector<int>& nums, int k) {
-        sort(nums.begin() , nums.end());
-        int cnt = 0;        
+        int ans = 0;
         int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            int j = i;
-            int s = i;
-            while(j < n && nums[j] == nums[i]) j++;
-            i = j - 1;
-            if(k == 0) {
-                if(i != s) cnt++;
-            } else {
-                int idx = bs(nums , i + 1 , n - 1 , nums[i] , k);
-                if(idx != -1) cnt++;
+        map <int , int> hash;
+        
+        if(k == 0) {
+            for(int i = 0; i < n; i++) {
+                hash[nums[i]]++;
             }
+            for(auto i: hash) {
+                if(i.second > 1) ans++;
+            }
+            return ans;
         }
         
-        return cnt;
+        for(int i = 0; i < n; i++) {
+            if(hash[nums[i]] != 0) continue;    
+            ans += hash[nums[i] - k] + hash[nums[i] + k];
+            hash[nums[i]]++;
+        }
+        
+        return ans;
     }
 };
