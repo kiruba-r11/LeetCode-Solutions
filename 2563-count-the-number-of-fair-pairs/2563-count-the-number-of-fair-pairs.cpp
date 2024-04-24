356ed8f89ee282a13910918e41184ac6c5c60bfa
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-04-24 09:53:54 +0530

    Time: 155 ms (27.60%), Space: 60 MB (81.97%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,26 +1,8 @@
 class Solution {
 public:
-    int bslb(vector <int> &nums , int key , int low , int high) {
-    
+    int find(vector <int> &nums , int key) {
         int n = nums.size();
-        int ans = n;
-        
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(nums[mid] >= key) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            } 
-        }
-        
-        return ans;
-    }
-    
-    int bsub(vector <int> &nums , int key , int low , int high) {
-        
-        int n = nums.size();
+        int low = 0 , high = n - 1;
         int ans = -1;
         
         while(low <= high) {
@@ -30,7 +12,19 @@ public:
                 low = mid + 1;
             } else {
                 high = mid - 1;
-            } 
+            }
+        }
+        
+        return ans;
+    }
+    
+    long long solve(vector <int> &nums , int limit) {
+    
+        int n = nums.size();
+        long long ans = 0;
+        for(int i = 0; i < n; i++) {
+            int idx = find(nums , limit - nums[i]);
+            if(idx > i) ans += idx - i;
         }
         
         return ans;
@@ -39,21 +33,10 @@ public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
         sort(nums.begin() , nums.end());
         
-        int n = nums.size();
-        long long ans = 0;
+        long long up = solve(nums , upper);
+        long long low = solve(nums , lower - 1);
         
-        for(int i = 0; i < n; i++) {
-            int lb = lower - nums[i];
-            int ub = upper - nums[i];
-            int li = bslb(nums , lb , i + 1 , n - 1);
-            int ui = bsub(nums , ub , i + 1 , n - 1);
-
-            int cnt = ui - li + 1;
-            ans += cnt >= 0 ? cnt : 0;
-        }
-        
-        return ans;
+        return up - low;
     }
-    
 };
 
