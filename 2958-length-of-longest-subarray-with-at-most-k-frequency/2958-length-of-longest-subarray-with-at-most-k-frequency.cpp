6934484566612ad6e40988b06651fb1fdf978feb
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-04-25 09:04:21 +0530

    Time: 402 ms (5.02%), Space: 148.5 MB (26.14%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,52 +1,18 @@
 class Solution {
 public:
-    bool ispossible(vector <int> &nums , int k , int allow) {
-        
-        int n = nums.size();
-        int i = 0 , j = 0;
-        unordered_map <int , int> hash;
-        
-        while(j < n) {
-            int window = j - i + 1;
-            if(window <= k) {
-                hash[nums[j]]++;
-                if(window == k) {
-                    bool check = true;
-                    for(auto elem: hash) {
-                        if(elem.second > allow) {
-                            check = false;
-                            break;
-                        }
-                    }
-                    if(check) return true;
-                }
-                j++;
-            } else {
-                if(hash[nums[i]] == 1) {
-                    hash.erase(nums[i]);
-                } else {
-                    hash[nums[i]]--;
-                }
-                i++;
-            }
-        }
-        return false;    
-    }
-    
     int maxSubarrayLength(vector<int>& nums, int k) {
         int n = nums.size();
-        int low = 0 , high = n;
-        int ans = low;
+        int i = 0 , j = 0 , ans = 0;
+        map <int , int> hash;
         
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            bool condition = ispossible(nums , mid , k);
-            if(condition) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        while(j < n) {
+            hash[nums[j]]++;
+            while(hash[nums[j]] > k) {
+                hash[nums[i]]--;
+                i++;
             }
+            if(hash[nums[j]] <= k) ans = max(ans , j - i + 1);
+            j++;
         }
         
         return ans;
