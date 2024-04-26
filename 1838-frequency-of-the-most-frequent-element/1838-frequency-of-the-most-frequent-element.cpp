4dfc32381e19e1c5174785cc0d4453d96519c416
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-04-26 09:18:16 +0530

    Time: 147 ms (86.20%), Space: 105.5 MB (70.65%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,47 +1,26 @@
 class Solution {
 public:
-    bool condition(vector <int> &nums , int k , int val) {
-    
-        int n = nums.size();
-        int i = 0 , j = 0;
-        long long sum = 0;
-        
-        while(j < n) {
-            int window = j - i + 1;
-            if(window <= k) {
-                sum += nums[j];
-                
-                if(window == k) {
-                    int last = nums[j];
-                    int cnt = k - 1;
-                    if((sum - last + val) >= cnt * 1LL * last) return true;
-                }
-                
-                j++;
-            } else {
-                sum -= nums[i];
-                i++;
-            }
-        }
-        
-        return false;
-    }
-    
-    
     int maxFrequency(vector<int>& nums, int k) {
-        int n = nums.size();
         sort(nums.begin() , nums.end());
         
-        int low = 1 , high = n;
+        int n = nums.size(); 
+        int i = 0 , j = 0;
+        long long sum = 0;
         int ans = 0;
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            if(condition(nums , mid , k)) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        
+        while(j < n) {
+            sum += nums[j];
+            long long increase = sum - nums[j]; // exclude jth element
+            long long cnt = j - i; // exclude jth element
+            while(increase + k < cnt * 1LL * nums[j]) {
+                sum -= nums[i];
+                i++;
+                increase = sum - nums[j];
+                cnt = j - i;
             }
+            
+            if(increase + k >= cnt * nums[j]) ans = max(ans , j - i + 1);
+            j++;
         }
         
         return ans;
