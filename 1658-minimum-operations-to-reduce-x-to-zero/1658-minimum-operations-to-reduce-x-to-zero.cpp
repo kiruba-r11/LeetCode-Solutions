f66b849efbed64ade72fa04743f5516c8bb58bc9
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-05-02 09:58:20 +0530

    Time: 122 ms (42.32%), Space: 100.9 MB (80.60%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
+        int total = 0;
         int n = nums.size();
-        int i = 0 , j = 0 , sum = 0 , ans = -1 , tsum = 0;
         
-        for(int k = 0; k < n; k++) tsum += nums[k];
+        for(int i = 0; i < n; i++) total += nums[i];
         
-        if(tsum < x) return -1;
+        int i = 0 , j = 0;
+        int sum = 0;
+        int ans = -1;
+        
+        if(total == x) return n;
         
         while(j < n) {
-            int cur = nums[j];
-            if((cur + sum) <= (tsum - x)) {
-                sum += cur;
-                if(sum == (tsum - x)) ans = max(ans , j - i + 1);
-                j++;
-            } else {
-                while((cur + sum) > (tsum - x)) {
-                    sum -= nums[i];
-                    i++;
-                }
+            sum += nums[j];
+            while(i < j && (total - sum) < x) {
+                sum -= nums[i];
+                i++;
             }
+            if((total - sum) == x) {
+                ans = max(ans , j - i + 1);
+                // cout << i << " " << j << endl;
+            }
+            j++;
         }
         
-        if(ans == -1) return -1;
-        return n - ans;
+        return ans == -1 ? -1 : n - ans;
     }
 };
