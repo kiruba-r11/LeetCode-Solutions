e92e93b8ebe82982eaee924686b5e29e55103049
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-05-06 09:48:37 +0530

    Time: 202 ms (5.28%), Space: 64.2 MB (6.38%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int slide(vector <int> nums , int k) {
-        
-        int i = 0 , j = 0;
+    int solve(vector <int> &nums , int k) {
+    
         int n = nums.size();
-        int ans = 0;
+        int i = 0 , j = 0;
         map <int , int> hash;
+        int ans = 0;
         
         while(j < n) {
             hash[nums[j]]++;
-            while(hash.size() > k) {
-                if(hash[nums[i]] == 1) hash.erase(nums[i]);
-                else hash[nums[i]]--;
+            while(i < j && hash.size() > k) {
+                hash[nums[i]]--;
+                if(hash[nums[i]] == 0) hash.erase(nums[i]);
                 i++;
             }
             if(hash.size() <= k) ans += j - i + 1;
             j++;
         }
-        
         return ans;
     }
     
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int n = nums.size();
-        return slide(nums , k) - slide(nums , k - 1);
+        return solve(nums , k) - solve(nums , k - 1);
     }
 };
