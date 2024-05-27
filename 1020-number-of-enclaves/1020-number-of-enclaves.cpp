623011f128f1de05a738aa63cbb9ee9b04824d40
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-05-27 09:45:04 +0530

    Time: 52 ms (53.71%), Space: 27.3 MB (99.75%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,50 +1,52 @@
 class Solution {
 public:
-    vector <pair <int , int>> dirs = {{0 , 1} , {0 , -1} , {1 , 0} , {-1 , 0}};
-    void dfs(vector <vector <int>> &grid , int row , int col) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        if(row < 0 || col < 0 || row >= n || col >= m || grid[row][col] == 0) return;
-        
-        grid[row][col] = 0;
-        for(auto dir: dirs) {
-            int x = dir.first + row;
-            int y = dir.second + col;
-            
-            dfs(grid , x , y);
-        }
-    }
-    
-    void helper_row(vector <vector <int>> &grid , int row) {
-        int m = grid[0].size();
-        for(int j = 0; j < m; j++) {
-            if(grid[row][j] == 1) {
-                dfs(grid , row , j);
-            }
-        }
-    }
-    
-    void helper_col(vector <vector <int>> &grid , int col) {
-        int n = grid.size();
-        for(int i = 0; i < n; i++) {
-            if(grid[i][col] == 1) {
-                dfs(grid , i , col);
-            }
-        }
-    }
-    
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         
-        helper_row(grid , 0);
-        helper_row(grid , n - 1);
-        helper_col(grid , 0);
-        helper_col(grid , m - 1);
+        queue <pair <int , int>> q;
+        for(int i = 0; i < n; i++) {
+            if(grid[i][0] == 1) {
+                q.push({i , 0});
+                grid[i][0] = 0;
+            }
+            if(grid[i][m - 1] == 1) {
+                q.push({i , m - 1});
+                grid[i][m - 1] = 0;
+            }
+        }
+        
+        for(int j = 0; j < m; j++) {
+            if(grid[0][j] == 1) {
+                q.push({0 , j});
+                grid[0][j] = 0;
+            }
+            if(grid[n - 1][j] == 1) {
+                q.push({n - 1 , j});
+                grid[n - 1][j] = 0;
+            }
+        }
+        
+        vector <pair <int , int>> dirs = {{0 , 1} , {0 , -1} , {1 , 0} , {-1 , 0}};
+        while(!q.empty()) {
+            int count = q.size();
+            for(int i = 0; i < count; i++) {
+                pair <int , int> node = q.front();
+                q.pop();
+                
+                for(auto dir: dirs) {
+                    int x = dir.first + node.first;
+                    int y = dir.second + node.second;
+                    
+                    if(x >= 0 && y >= 0 && x < n && y < m && grid[x][y] == 1) {
+                        grid[x][y] = 0;
+                        q.push({x , y});
+                    }
+                }
+            }
+        }
         
         int ans = 0;
-        
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == 1) ans++;
