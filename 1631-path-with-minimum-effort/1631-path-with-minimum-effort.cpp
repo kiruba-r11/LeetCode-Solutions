9643185e2b77da0b8f0b786af1cd3d80d155d568
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-06-06 10:14:39 +0530

    Time: 175 ms (21.24%), Space: 37.6 MB (25.23%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    vector <pair <int , int>> dirs = {{-1 , 0} , {1 , 0} , {0 , 1} , {0 , -1}};
-    
-    bool solve(vector <vector <int>> &heights , int row , int col , int range , int prev , vector <vector <bool>> &visited) {
-
-        if(row < 0 || row >= heights.size() || col < 0 || col >= heights[0].size() || visited[row][col] == true) return false;
-        if(abs(prev - heights[row][col]) > range) return false;
-        
-        if(row == heights.size() - 1 && col == heights[0].size() - 1) return true;
-        
-        visited[row][col] = true;
-        for(auto dir: dirs) {
-            int x = dir.first;
-            int y = dir.second;
-            
-            if(solve(heights , row + x , col + y , range , heights[row][col] , visited)) return true;
-        }
-        
-        // visited[row][col] = false;
-        return false;
-    }
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int low = 0 , high = 1e6;
-        int ans = high;
         int n = heights.size();
         int m = heights[0].size();
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            vector <vector <bool>> visited(n , vector <bool> (m));
-            bool check = solve(heights , 0 , 0 , mid , heights[0][0] , visited);
-            if(check) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        
+        vector <vector <int>> dist(n , vector <int> (m , INT_MAX));
+        vector <pair <int , int>> dirs = {{0 , 1} , {0 , -1} , {1 , 0} , {-1 , 0}};
+        priority_queue <vector <int> , vector <vector <int>> , greater <vector <int>>> pq;
+        
+        pq.push({0 , 0 , 0});
+        dist[0][0] = 0;        
+        
+        while(!pq.empty()) {
+            int nd = pq.top()[0];
+            int ni = pq.top()[1];
+            int nj = pq.top()[2];
+            
+            pq.pop();
+            
+            if(ni == n - 1 && nj == m - 1) return dist[n - 1][m - 1];
+            
+            for(auto dir: dirs) {
+                int x = dir.first + ni;
+                int y = dir.second + nj;
+                
+                if(x >= 0 && x < n && y >= 0 && y < m) {
+                    int d = max(abs(heights[x][y] - heights[ni][nj]) , nd);
+                    if(d < dist[x][y]) {
+                        dist[x][y] = d;
+                        pq.push({d , x , y});
+                    }
+                }
             }
         }
         
-        return ans;
+        return dist[n - 1][m - 1];
     }
 };
