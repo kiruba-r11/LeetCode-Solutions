be21322ee6e9237846a666bee4385b9e9224507a
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-08-21 19:43:09 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (48.78%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    void permutation(vector <int> &nums , vector <int> &cur , vector <vector <int>> &ans) {
+    void solve(vector <int> &nums , int idx , vector <int> &cur , vector <vector <int>> &ans) {
+        int n = nums.size();
         
-        if(nums.size() == cur.size()) {
+        if(idx == n) {
             ans.push_back(cur);
             return;
         }
-        
-        int n = nums.size();
+            
         for(int i = 0; i < n; i++) {
             if(nums[i] != -11) {
-                
-                // make effort
                 int temp = nums[i];
                 nums[i] = -11;
+                
                 cur.push_back(temp);
-                
-                permutation(nums , cur , ans);
-                
-                // undo effort
+                solve(nums , idx + 1 , cur , ans);
                 cur.pop_back();
+                
                 nums[i] = temp;
             }
         }
+        
     }
-    
     vector<vector<int>> permute(vector<int>& nums) {
-        vector <vector <int>> ans;
         vector <int> cur;
-        permutation(nums , cur , ans);
-        return ans;    
+        vector <vector <int>> ans;
+        solve(nums , 0 , cur , ans);
+        return ans;
     }
 };
