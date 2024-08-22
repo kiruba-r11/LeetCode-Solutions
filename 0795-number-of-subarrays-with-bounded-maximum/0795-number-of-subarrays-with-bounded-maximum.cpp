3471f40ee2d5d50cd3d6cc8d123295bdc5ba35a9
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-08-22 19:37:00 +0530

    Time: 55 ms (18.48%), Space: 55 MB (33.16%) - LeetHub

--- 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.cpp
+++ 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int find(vector <int> &nums , int range) {
+    int solve(vector <int> &nums , int range) {
         int n = nums.size();
+        int prefix_cnt = 0;
         int ans = 0;
-        int prev_invalid = -1;
-        
         for(int i = 0; i < n; i++) {
-            if(nums[i] > range) prev_invalid = i;
-            else ans += i - prev_invalid;
+            if(nums[i] <= range) {
+                prefix_cnt++;
+            } else {
+                prefix_cnt = 0;
+            }
+            ans += prefix_cnt;
         }
         
         return ans;
     }
     
     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
-
-        // Better Intuition for problems with [left , right] range
-        // Find [right] (<= right)
-        // Find [left - 1] (<= left)
-        // Find [right] - [left - 1] ([left , right])
+        int l = solve(nums , left - 1);
+        int r = solve(nums , right);
         
-        int r = find(nums , right);
-        int l = find(nums , left - 1);
-        
-        int ans = r - l;
-        
-        return ans;
+        return r - l;
     }
 };
