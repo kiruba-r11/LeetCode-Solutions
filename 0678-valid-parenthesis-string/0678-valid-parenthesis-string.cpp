40c5276633a537fb8442af7fb317f8ffd1990da1
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-08-27 07:38:26 +0530

    Time: 8 ms (9.14%), Space: 9.7 MB (12.24%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    bool solve(string &s , int i , int open , int close , vector <vector <vector <int>>> &dp) {
+    bool solve(string &s , int i , int open , vector <vector <int>> &dp) {
         
-        if(close > open) return false;
+        if(open < 0) return false;
         
         if(i == s.size()) {
-            if(open == close) return true;
+            if(open == 0) return true;
             return false;
         }
         
-        if(dp[i][open][close] != -1) return dp[i][open][close];
+        if(dp[i][open] != -1) return dp[i][open];
         
-        bool open_n = solve(s , i + 1 , open + 1 , close , dp);
-        bool close_n = solve(s , i + 1 , open , close + 1 , dp);
-        bool empty_n = solve(s , i + 1 , open , close , dp);
+        bool open_n = solve(s , i + 1 , open + 1 , dp);
+        bool close_n = solve(s , i + 1 , open - 1 , dp);
+        bool empty_n = solve(s , i + 1 , open , dp);
         
-        if(s[i] == '(') return dp[i][open][close] = open_n;
-        else if(s[i] == ')') return dp[i][open][close] = close_n;
+        if(s[i] == '(') return dp[i][open] = open_n;
+        else if(s[i] == ')') return dp[i][open] = close_n;
         
-        return dp[i][open][close] = open_n || close_n || empty_n;
+        return dp[i][open] = open_n || close_n || empty_n;
     }
     
     bool checkValidString(string s) {
         int n = s.size();
-        vector <vector <vector <int>>> dp(n + 1 , vector <vector <int>> (n + 1 , vector <int> (n + 1 , -1)));
+        vector <vector <int>> dp(n + 1 , vector <int> (n + 1 , -1));
         
-        return solve(s , 0 , 0 , 0 , dp);
+        return solve(s , 0 , 0 , dp);
     }
 };
