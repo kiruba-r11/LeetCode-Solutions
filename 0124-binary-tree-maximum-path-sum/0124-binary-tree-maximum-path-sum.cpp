84df5be6797f7e4b6dac01ccd6a317ad2fbf5369
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-09-02 19:11:03 +0530

    Time: 12 ms (92.56%), Space: 27.7 MB (10.54%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -12,31 +12,17 @@
 class Solution {
 public:
     int ans = -1001;
-    // {extendible path, non-extendible path}
     pair <int , int> solve(TreeNode* root) {
         if(!root) return {-1001 , -1001};
-        if(!root->left && !root->right) {
-            ans = max(ans , root->val);
-            return {root->val , root->val};
-        }
         
         pair <int , int> left = solve(root->left);
         pair <int , int> right = solve(root->right);
         
         pair <int , int> cur;
-        
-        // Extendible path for cur:
-        // max(left extendible path + cur_sum , right extendible path + cur_sum , cur_sum)
         cur.first = max(max(left.first , right.first) + root->val , root->val);
-        
-        
-        // Non-Extendible path for cur:
-        // max( left non-extendible path , 
-        //      right non-extendible path ,
-        //      left extendible path + right extendible path + cur_sum)
         cur.second = max(max(left.second , right.second) , left.first + right.first + root->val);
         
-        ans = max(ans , max(cur.first , cur.second));
+        ans = max(max(cur.first , cur.second) , ans);
         
         return cur;
     }
