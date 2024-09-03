176c0f3ec3386aee903e5f768eb397eeeea02eff
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-09-03 20:47:04 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (53.35%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    void subset(vector <int> &nums , int i , vector <int> &cur , vector <vector <int>> &ans) {
-            
-        if(i == nums.size()) {
-            ans.push_back(cur);
-            return;
+    int fpow(int base , int power) {
+        if(power == 0) return 1;
+        if(power & 1) return fpow(base * base , power / 2) * base;
+        return fpow(base * base , power / 2);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector <vector <int>> ans;
+        
+        for(int i = 0; i < fpow(2 , n); i++) {
+            vector <int> temp_ans;
+            for(int j = 0; j < n; j++) {
+                if(i & (1 << j)) {
+                    temp_ans.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp_ans);
         }
         
-        // exclude element
-        subset(nums , i + 1 , cur , ans);
-        
-        // make effort
-        cur.push_back(nums[i]);
-        
-        // include element
-        subset(nums , i + 1 , cur , ans);
-        
-        // undo effect
-        cur.pop_back();
-        
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector <vector <int>> ans;
-        vector <int> cur;
-        subset(nums , 0 , cur , ans);
         return ans;
     }
 };
