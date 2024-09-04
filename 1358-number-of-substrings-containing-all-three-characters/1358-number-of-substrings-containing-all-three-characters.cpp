d7835f75095599650c6b1f791faf1825f95726eb
Kirubanand R <76843281+kiruba-r11@users.noreply.github.com>
2024-09-04 17:39:26 +0530

    Time: 59 ms (25.37%), Space: 22.3 MB (5.07%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+        map <char , int> hash;
+        int n = s.size(); 
         int i = 0 , j = 0;
-        int n = s.size();
         int ans = 0;
-        map <int , int> hash;
-        
         while(j < n) {
-            hash[s[j] - 'a']++;
+            hash[s[j]]++;
             while(hash.size() == 3) {
-                if(hash[s[i] - 'a'] == 1) {
-                    break;
-                } else {
-                    hash[s[i] - 'a']--;
-                    i++;
-                }
+                hash[s[i]]--;
+                if(hash[s[i]] == 0) hash.erase(s[i]);
+                i++;
             }
-            if(hash.size() == 3) {
-                ans += i + 1;
-            }
+            if(hash.size() != 3) ans += j - i + 1;
             j++;
         }
-        
-        return ans;
+
+        int total = n * 1LL * (n + 1) / 2 - ans;
+        return total;
     }
 };
+
+
